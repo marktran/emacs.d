92e9867c6eafca7739e92534845573879caaba77
Mark Tran <mark@nirv.net>
2008-10-15 05:18:16 -0400

    Added bindings from other init files

--- init/init-keybindings.el
+++ init/init-keybindings.el
@@ -2,6 +2,17 @@
 
 ;;; .emacs.d/init/init-keybindings.el : Mark Tran <mark@nirv.net>
 
+(global-set-key [f1] 'w3m)
+(global-set-key [f2] 'gnus)
+(global-set-key [f3] 'switch-to-scratch-or-previous)
+(global-set-key [f4] 'replace-regexp)
+(global-set-key [f5] 'kmacro-end-or-call-macro)
+(global-set-key [f6] 'slime)
+
+(global-set-key [(shift f4)] 'kmacro-start-or-end)
+(global-set-key [(tab)] 'smart-tab)
+
+(global-set-key (kbd "C-c l") 'copy-line)
 (global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
 
 ;; window movement
