32176648514aba38c9f80e74ea45266f2d6744c5
Mark Tran <mark@nirv.net>
2008-12-15 12:56:44 -0600

    Split off settings for built-in modes to init-modes.el

--- init/init-libraries.el
+++ init/init-libraries.el
@@ -6,37 +6,6 @@
 (require 'browse-kill-ring)
 (browse-kill-ring-default-keybindings)
 
-;; ediff
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
-(setq ediff-split-window-function 'split-window-horizontally)
-
-(add-hook 'ediff-cleanup-hook (lambda () (ediff-janitor nil nil)))
-
-;; erc
-(setq erc-user-full-name "Mark T."
-      erc-email-userid "tran"
-      erc-nick '("mqt" "qvt")
-      erc-server "irc.freenode.net"
-      erc-port 6667
-      erc-prompt-for-password nil)
-
-;; eshell
-(setq eshell-ls-initial-args "-F"
-      eshell-ls-use-colors nil)
-
-(setq eshell-prompt-function
-  (lambda ()
-    (concat "(" (eshell/pwd) ")"
-      (if (= (user-uid) 0) " # " " % "))))
-
-;; ido
-(ido-mode t)
-(setq ido-enable-flex-matching t)
-
-(add-hook 'ido-setup-hook 
-          (lambda () 
-            (define-key ido-completion-map [tab] 'ido-complete)))
-
 ;; lisppaste
 (autoload 'lisppaste-paste-region "lisppaste"
   "Send region to paste.lisp.org" t)
@@ -46,11 +15,6 @@
 
 ;; magit
 (autoload 'magit-status "magit" "Interface to Git" t)
-
-;; makefile
-(add-hook 'makefile-mode-hook 
-  (lambda()
-    (setq show-trailing-whitespace t)))
 
 ;; paredit
 (autoload 'paredit-mode "paredit-beta" 
---
+++ init/init-modes.el
@@ -0,0 +1,41 @@
+;;; -*- Mode: Emacs-Lisp; -*-
+
+;;; .emacs.d/init/init-modes.el : Mark Tran <mark@nirv.net>
+
+;; ediff
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+(setq ediff-split-window-function 'split-window-horizontally)
+
+(add-hook 'ediff-cleanup-hook (lambda () (ediff-janitor nil nil)))
+
+;; erc
+(setq erc-user-full-name "Mark T."
+      erc-email-userid "tran"
+      erc-nick '("mqt" "qvt")
+      erc-server "irc.freenode.net"
+      erc-port 6667
+      erc-prompt-for-password nil)
+
+;; eshell
+(setq eshell-ls-initial-args "-F"
+      eshell-ls-use-colors nil)
+
+(setq eshell-prompt-function
+  (lambda ()
+    (concat "(" (eshell/pwd) ")"
+      (if (= (user-uid) 0) " # " " % "))))
+
+;; ido
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+
+(add-hook 'ido-setup-hook 
+          (lambda () 
+            (define-key ido-completion-map [tab] 'ido-complete)))
+
+;; makefile
+(add-hook 'makefile-mode-hook 
+  (lambda()
+    (setq show-trailing-whitespace t)))
+
+(provide 'init-modes)
