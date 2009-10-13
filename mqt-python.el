e633a5c2ae785304cfa7afcacdb1d56195efd0b9
Mark Tran <mark@nirv.net>
2009-10-12 20:34:33 -0500

    removed eldoc mode for python

--- mqt-python.el
+++ mqt-python.el
@@ -7,8 +7,6 @@
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (require 'python-mode)
 
-(add-hook 'python-mode-hook 'turn-on-eldoc-mode)
-
 ;; ipython
 (setq ipython-command "/opt/local/Library/Frameworks/Python.framework/Versions/2.6/bin/ipython"
       py-python-command-args '("-colors" "NoColor"))
