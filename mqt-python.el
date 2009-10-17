529c0625148fd3f0523ddb1276ef7c2c905e0172
Mark Tran <mark@nirv.net>
2009-10-17 17:27:56 -0500

    added flymake+pyflakes syntax checking

--- mqt-python.el
+++ mqt-python.el
@@ -2,14 +2,26 @@
 
 ;;; .emacs.d/mqt-python.el : Mark Tran <mark@nirv.net>
 
-;; python-mode
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-(require 'python-mode)
+(autoload 'python-mode "python-mode" t)
+(eval-after-load 'python-mode
+  '(require 'ipython))
 
-;; ipython
 (setq ipython-command "/opt/local/Library/Frameworks/Python.framework/Versions/2.6/bin/ipython"
       py-python-command-args '("-colors" "NoColor"))
-(require 'ipython)
+
+;; syntax checking with flymakes and pyflakes
+(when (load "flymake" t) 
+  (defun flymake-pyflakes-init () 
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+                       'flymake-create-temp-inplace)) 
+           (local-file (file-relative-name 
+                        temp-file 
+                        (file-name-directory buffer-file-name)))) 
+      (list "pyflakes-2.6" (list local-file)))) 
+
+  (add-to-list 'flymake-allowed-file-name-masks 
+               '("\\.py\\'" flymake-pyflakes-init))) 
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
 
 (provide 'mqt-python)
