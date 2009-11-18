6e7768d724400f3bafc7ebc0d09aa6464bb94d95
Mark Tran <mark@nirv.net>
2009-11-18 11:06:03 -0600

    added tv show keywords for sceneaccess pre channel, various other settings

--- .ercrc.el
+++ .ercrc.el
@@ -5,21 +5,52 @@
 (setq erc-auto-query 'buffer
       erc-button-buttonize-nicks nil
       erc-current-nick-highlight-type 'nick
+      erc-default-server "lambda.nirv.net"
+      erc-default-port 65535
+      erc-disable-ctcp-replies t
       erc-header-line-format nil
       erc-hide-list '("JOIN" "PART" "NICK" "QUIT")
       erc-insert-timestamp-function 'erc-insert-timestamp-left
+      erc-keyword-highlight-type 'all
       erc-kill-buffer-on-part t
       erc-kill-queries-on-quit t
       erc-kill-server-buffer-on-quit t
       erc-mode-line-format "%t"
       erc-notice-prefix "* "
       erc-prompt ">"
+      erc-prompt-for-password nil
       erc-server-auto-reconnect nil
       erc-timestamp-format "%H:%M "
       erc-timestamp-only-if-changed-flag nil
       erc-track-exclude-server-buffer t
       erc-track-exclude-types '("JOIN" "MODE" "NICK" "PART" "QUIT")
-      erc-track-use-faces nil)
+      erc-track-faces-priority-list '(erc-current-nick-face
+                                      erc-error-face
+                                      erc-keyword-face
+                                      erc-pal-face)
+      erc-track-priority-faces-only '("#python"
+                                      "#scc-pre")
+      erc-whowas-on-nosuchnick t)
+
+(setq erc-keywords
+      (mapcar (lambda (x) (concat "^\\[PRE\\] \\[ TV \\] \\[ " x))
+              '("Californication"
+                "Curb.Your.Enthusiasm"
+                "Dexter"
+                "Doctor.Who"
+                "Entourage"
+                "Heroes"
+                "How.I.Met.Your.Mother"
+                "Its.Always.Sunny.In.Philadelphia"
+                "Jeopardy"
+                "Mad.Men"
+                "Man.v.Food"
+                "Monk"
+                "Sons.of.Anarchy"
+                "The.Big.Bang.Theory"
+                "The.IT.Crowd"
+                "The.Office"
+                "Top.Gear")))
 
 (add-hook 'erc-mode-hook 'erc-add-scroll-to-bottom)
 (add-hook 'erc-mode-hook 'turn-on-visual-line-mode)
