9b81341254b4feea9be2f0836c04a2fc642e2158
Mark Tran <mark@nirv.net>
2009-11-17 22:37:58 -0600

    refactored

--- mqt-ruby.el
+++ mqt-ruby.el
@@ -2,15 +2,35 @@
 
 ;;; .emacs.d/mqt-ruby.el : Mark Tran <mark@nirv.net>
 
+(autoload 'inf-ruby "inf-ruby" nil t)
+(autoload 'inf-ruby-keys "inf-ruby" nil t)
+(autoload 'rhtml-mode "rhtml-mode" nil t)
+(autoload 'ri "ri-ruby.el" nil t)
+(autoload 'ruby-electric-mode "ruby-electric")
 (autoload 'ruby-mode "ruby-mode" nil t)
+(autoload 'textmate-goto-file "textmate" nil t)
+(autoload 'xmp "rcodetools" nil t)
+
+(setq erb-type-to-delim-face nil
+      erb-type-to-face nil
+      ri-ruby-script (expand-file-name "~/.emacs.d/vendor/ri-emacs.rb")
+      rinari-major-modes (list 'css-mode-hook
+                               'dired-mode-hook
+                               'javascript-mode-hook
+                               'mumamo-after-change-major-mode-hook
+                               'ruby-mode-hook
+                               'yaml-mode-hook)
+      ruby-electric-expand-delimiters-list nil)
+
+(add-to-list 'auto-mode-alist '("\\.html\\.erb" . rhtml-mode))
 
 (eval-after-load 'ruby-mode
   '(define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent))
 
-;; electric
-(autoload 'ruby-electric-mode "ruby-electric")
-(setq ruby-electric-expand-delimiters-list nil)
-
+(add-hook 'rinari-minor-mode-hook
+          (lambda ()
+            (define-key rinari-minor-mode-map (kbd "C-t") 'textmate-goto-file)))
+(add-hook 'ruby-mode-hook 'inf-ruby-keys)
 (add-hook 'ruby-mode-hook 'ruby-electric-mode)
 
 ;; flymake
@@ -40,26 +60,5 @@
             (if (and (not (null buffer-file-name))
                      (file-writable-p buffer-file-name))
                 (flymake-ruby-load))))
-
-;; inferior ruby
-(autoload 'inf-ruby "inf-ruby" nil t)
-(autoload 'inf-ruby-keys "inf-ruby" nil t)
-
-(add-hook 'ruby-mode-hook 'inf-ruby-keys)
-
-;; rcodetools
-(autoload 'xmp "rcodetools" nil t)
-
-;; ri
-(autoload 'ri "ri-ruby.el" nil t)
-(setq ri-ruby-script (expand-file-name "~/.emacs.d/vendor/ri-emacs.rb"))
-
-;; rinari
-(setq rinari-major-modes (list 'css-mode-hook
-                               'dired-mode-hook
-                               'javascript-mode-hook
-                               'mumamo-after-change-major-mode-hook
-                               'ruby-mode-hook
-                               'yaml-mode-hook))
 
 (provide 'mqt-ruby)
