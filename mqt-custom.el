fa2af0ab78ed948bdda305b6be43835abfad3f22
Mark Tran <mark@nirv.net>
2010-02-25 16:54:48 -0800

    some sort of autogen change

--- mqt-custom.el
+++ mqt-custom.el
@@ -7,10 +7,11 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(canlock-password "491e5b496d78341c76e90c963035514f6add2780")
  '(safe-local-variable-values (quote ((Syntax . Common-Lisp)))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-)
+ )
