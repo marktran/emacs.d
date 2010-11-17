71b7d7a267b80bb7c3b513b2a853fa1fccaa9f1d
Mark Tran <mark.tran@gmail.com>
2010-11-17 13:45:53 -0800

    added revert-all-buffers function from plover

--- mqt-functions.el
+++ mqt-functions.el
@@ -254,6 +254,21 @@ comment-dwim, when it inserts comment at the end of the line."
       (find-file (cdr (assoc filename
                              file-assoc-list))))))
 
+;; http://blog.plover.com/prog/revert-all.html
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files"
+  (interactive)
+  (let* ((list (buffer-list))
+         (buffer (car list)))
+    (while buffer
+      (when (and (buffer-file-name buffer) 
+                 (not (buffer-modified-p buffer)))
+        (set-buffer buffer)
+        (revert-buffer t t t))
+      (setq list (cdr list))
+      (setq buffer (car list))))
+  (message "Refreshed open files"))
+
 ;; http://atomized.org/2010/06/ \
 ;; resolving-merge-conflicts-the-easy-way-with-smerge-kmacro/
 (defun sm-try-smerge ()
