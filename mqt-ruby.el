20753e03dc71a6a1f3c029e0112959197e4d4822
Mark Tran <mark.tran@gmail.com>
2010-12-25 14:03:12 -0600

    removed unused modes/settings

--- mqt-ruby.el
+++ mqt-ruby.el
@@ -3,21 +3,13 @@
 (autoload 'inf-ruby "inf-ruby" nil t)
 (autoload 'inf-ruby-keys "inf-ruby" nil t)
 (autoload 'rhtml-mode "rhtml-mode" nil t)
-;; (autoload 'ri "ri-ruby.el" nil t)
 (autoload 'rspec-mode "rspec-mode" nil t)
-;; (autoload 'xmp "rcodetools" nil t)
 
 (setq erb-type-to-delim-face nil
       erb-type-to-face nil
-      ri-ruby-script (expand-file-name "~/.emacs.d/vendor/ri-emacs.rb")
-      rinari-major-modes (list 'css-mode-hook
-                               'dired-mode-hook
-                               'javascript-mode-hook
-                               'mumamo-after-change-major-mode-hook
-                               'ruby-mode-hook
-                               'yaml-mode-hook)
       rspec-spec-command "chdir /Users/mark/code/crowdflower/builder && bin/spec"
       rspec-use-rake-flag nil
+      ruby-deep-arglist nil
       ruby-electric-expand-delimiters-list nil)
 
 (add-to-list 'auto-mode-alist '("\\.html\\.erb" . rhtml-mode))
