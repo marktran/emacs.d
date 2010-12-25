1cf7e237c1761ffe78f808faf757aa4e59d6d9bc
Mark Tran <mark.tran@gmail.com>
2010-12-25 14:01:56 -0600

    switched to more el-get included recipes

--- mqt-el-get.el
+++ mqt-el-get.el
@@ -5,14 +5,14 @@
 (require 'el-get)
 
 (setq el-get-sources
-      '(apel autopair color-theme django-mode el-get emacs-w3m magit nxhtml
-             package pymacs python-mode smex switch-window textile-mode
-             undo-tree yasnippet
+      '(apel autopair color-theme coffee-mode diminish dired+ django-mode
+             el-get emacs-w3m magit markdown-mode mode-compile nxhtml package
+             paredit pymacs python-mode quack smex switch-window textile-mode
+             undo-tree yaml-mode yasnippet
              
              ;; elpa
              (:name clojure-mode :type elpa)
              (:name dired-isearch :type elpa)
-             (:name full-ack :type elpa)
              (:name gist :type elpa)
              (:name haml-mode :type elpa)
              (:name htmlize :type elpa)
@@ -28,49 +28,29 @@
              (:name sass-mode :type elpa)
              (:name slime :type elpa)
              (:name textmate :type elpa)
-             (:name yaml-mode :type elpa)
 
              ;; emacswiki
              (:name browse-kill-ring :type emacswiki)
-             (:name dired+ :type emacswiki)
 
              ;; other
-             (:name coffee-mode
-                    :type git
-                    :url "git://github.com/sstephenson/coffee-mode.git")
              (:name color-theme-chocolate-rain
                     :type git
                     :url "git://github.com/marktran/color-theme-chocolate-rain.git"
                     :load "color-theme-chocolate-rain.el"
                     :after (lambda () (color-theme-chocolate-rain)))
-             (:name diminish
-                    :type http
-                    :url "http://www.eskimo.com/~seldon/diminish.el")
+             (:name full-ack
+                    :type git
+                    :url "git://github.com/nschum/full-ack.git")
              (:name growl
                     :type http
                     :url "http://edward.oconnor.cx/elisp/growl.el")
              (:name ipython
                     :type http
                     :url "http://ipython.scipy.org/dist/ipython.el")
-             (:name markdown-mode
-                    :type git
-                    :url "git://jblevins.org/git/markdown-mode.git")
-             (:name mo-git-blame
-                    :type git
-                    :url "git://github.com/voins/mo-git-blame.git")
-             (:name mode-compile
-                    :type http
-                    :url "http://perso.tls.cena.fr/boubaker/distrib/mode-compile.el")
-             (:name paredit
-                    :type http
-                    :url "http://mumble.net/~campbell/emacs/paredit.el")
              (:name peepopen
                     :type git
                     :url "git://github.com/topfunky/PeepOpen-EditorSupport.git"
                     :features peepopen)
-             (:name quack
-                    :type http
-                    :url "http://www.neilvandyke.org/quack/quack.el")
              (:name rhtml-mode
                     :type git
                     :url "git://github.com/eschulte/rhtml.git"
@@ -79,7 +59,10 @@
                     :type git
                     :url "git://github.com/pezra/rspec-mode.git"
                     :features rspec-mode
-                    :compile "rspec-mode.el")))
+                    :compile "rspec-mode.el")
+             (:name smart-tab
+                    :type git
+                    :url "git://github.com/genehack/smart-tab.git")))
 
 (el-get)
 
