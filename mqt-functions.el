896d791530f193721f1c1f9f310da86f4b9105ef
Mark Tran <mark.tran@gmail.com>
2011-01-10 12:01:12 -0800

    recreate scratch buffer if it's killed

--- mqt-functions.el
+++ mqt-functions.el
@@ -240,7 +240,28 @@ comment-dwim, when it inserts comment at the end of the line."
      (list (line-beginning-position)
            (line-beginning-position 2)))))
 
-;;
+;; 
+(save-excursion
+  (set-buffer (get-buffer-create "*scratch*"))
+  (lisp-interaction-mode)
+  (make-local-variable 'kill-buffer-query-functions)
+  (add-hook 'kill-buffer-query-functions 'kill-scratch-buffer))
+
+(defun kill-scratch-buffer ()
+  ;; The next line is just in case someone calls this manually
+  (set-buffer (get-buffer-create "*scratch*"))
+  ;; Kill the current (*scratch*) buffer
+  (remove-hook 'kill-buffer-query-functions 'kill-scratch-buffer)
+  (kill-buffer (current-buffer))
+  ;; Make a brand new *scratch* buffer
+  (set-buffer (get-buffer-create "*scratch*"))
+  (lisp-interaction-mode)
+  (make-local-variable 'kill-buffer-query-functions)
+  (add-hook 'kill-buffer-query-functions 'kill-scratch-buffer)
+  ;; Since we killed it, don't let caller do that.
+  nil)
+
+;; http://www.emacswiki.org/emacs/RecreateScratchBuffer
 (defun kmacro-start-or-end (arg)
   "Toggle recording of keyboard macro."
   (interactive "P")
