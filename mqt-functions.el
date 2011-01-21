163fb856092eddc31a5511873a6fa82dc5a23ad9
Mark Tran <mark.tran@gmail.com>
2011-01-21 05:02:22 -0800

    helper macros for setting directory local variables (with implied safety)

--- mqt-functions.el
+++ mqt-functions.el
@@ -15,6 +15,39 @@
                 (let ((mark-even-if-inactive transient-mark-mode))
                   (indent-region (region-beginning) (region-end) nil))))))
 
+;; http://atomized.org/2009/05/emacs-23-easier-directory-local-variables/
+(defmacro absolute-dirname (path)
+  "Return the directory name portion of a path.
+
+If PATH is local, return it unaltered.
+If PATH is remote, return the remote diretory portion of the path."
+  `(cond ((tramp-tramp-file-p ,path)
+          (elt (tramp-dissect-file-name ,path) 3))
+         (t ,path)))
+
+(defmacro dir-locals (dir vars)
+  "Set local variables for a directory.
+
+DIR is the base diretory to set variables on.
+
+VARS is an alist of variables to set on files opened under DIR,
+in the same format as `dir-locals-set-class-variables' expects."
+  `(let ((name (intern (concat "dir-locals-"
+                               ,(md5 (expand-file-name dir)))))
+         (base-dir ,dir)
+         (base-abs-dir ,(absolute-dirname dir)))
+     (dir-locals-set-class-variables name ,vars)
+     (dir-locals-set-directory-class ,dir name nil)))
+
+(defmacro dir-locals-safe (directory variables)
+  "Set local variables for a directory and add variables to 
+safe-local-variable-values."
+  `(progn
+     (dir-locals ,directory ,variables)
+     (dolist (class ,variables)
+       (dolist (variable (cdr class))
+         (add-to-list 'safe-local-variable-values variable)))))
+
 ;; using IDO for bookmarks and recent files
 ;; http://blog.kelsin.net/2010/04/22/using-ido-for-bookmarks-and-recent-files/
 (defun bookmark-ido-find-file ()
