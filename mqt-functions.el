b752c0a959e10b273c1c22c82ae8e0dc48784e55
Mark Tran <mark.tran@gmail.com>
2011-04-02 03:52:43 -0700

    yegge's rename file and buffer function

--- mqt-functions.el
+++ mqt-functions.el
@@ -414,6 +414,22 @@ comment-dwim, when it inserts comment at the end of the line."
       (find-file (cdr (assoc filename
                              file-assoc-list))))))
 
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
 ;; http://blog.plover.com/prog/revert-all.html
 (defun revert-all-buffers ()
   "Refreshes all open buffers from their respective files"
