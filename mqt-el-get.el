90fa1a0d654af3bfb72d640ca9888d07cb37f450
Mark Tran <mark.tran@gmail.com>
2011-04-09 04:00:13 -0700

    el-get refactor

--- mqt-el-get.el
+++ mqt-el-get.el
@@ -7,24 +7,60 @@
 (require 'el-get)
 
 (setq el-get-sources
-      '(autopair browse-kill-ring coffee-mode color-theme
-                 color-theme-chocolate-rain diminish dired+ django-mode
-                 el-expectations el-get emacs-w3m escreen full-ack gist growl
-                 haml-mode ipython json magit markdown-mode mode-compile nxhtml
-                 package paredit pymacs python-mode quack rhtml-mode rspec-mode
-                 ruby-compilation ruby-mode rvm sass-mode scratch session
-                 smart-tab smex switch-window textile-mode undo-tree yaml-mode
-                 yari yasnippet
-             
-                 ;; elpa
-                 (:name dired-isearch :type elpa)
-                 (:name htmlize :type elpa)
-                 (:name inf-ruby :type elpa)
-                 (:name js2-mode :type elpa)
-                 (:name kill-ring-search :type elpa)
-                 (:name ruby-electric :type elpa)
-                 (:name slime :type elpa)
-                 (:name textmate :type elpa)))
+      '(autopair
+        browse-kill-ring
+        coffee-mode
+        color-theme
+        color-theme-chocolate-rain
+        color-theme-solarized
+        diminish
+        dired+
+        el-expectations
+        el-get
+        emacs-w3m
+        escreen
+        full-ack
+        gist
+        growl
+        haml-mode
+        highlight-indentation
+        ipython
+        json
+        magit
+        markdown-mode
+        mode-compile
+        nxhtml
+        package
+        paredit
+        pymacs
+        python-mode
+        quack
+        rhtml-mode
+        rspec-mode
+        ruby-compilation
+        ruby-mode
+        rvm
+        sass-mode
+        scratch
+        session
+        smart-tab
+        smex
+        switch-window
+        textile-mode
+        undo-tree
+        yaml-mode
+        yari
+        yasnippet
+        
+        ;; elpa
+        (:name dired-isearch :type elpa)
+        (:name htmlize :type elpa)
+        (:name inf-ruby :type elpa)
+        (:name js2-mode :type elpa)
+        (:name kill-ring-search :type elpa)
+        (:name ruby-electric :type elpa)
+        (:name slime :type elpa)
+        (:name textmate :type elpa)))
 
 (el-get)
 
