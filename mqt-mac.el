bbfae839d6273419664659bd4877a25fd6645af7
Mark Tran <mark.tran@gmail.com>
2012-03-19 17:37:18 -0700

    make copy/paste work in terminal

--- mqt-mac.el
+++ mqt-mac.el
@@ -22,4 +22,16 @@
   (interactive (browse-url-interactive-arg "URL: "))
   (start-process (concat "open " url) nil "open" "-g" url))
 
+(defun copy-from-osx ()
+   (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+   (let ((process-connection-type nil))
+        (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+               (process-send-string proc text)
+                     (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
 (provide 'mqt-mac)
