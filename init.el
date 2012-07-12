2e5e2932c64d72457d494d0309aba8f74d9f630d
Mark Tran <mark.tran@gmail.com>
2012-07-12 14:18:31 -0700

    remove ansi vector

--- init.el
+++ init.el
@@ -20,10 +20,7 @@
                 "/bin" ":"
                 "/usr/local/lib/node_modules/.bin/"))
 
-;; set ansi colors before ansi-color-map is built somewhere in an el-get package
-(setq ansi-color-names-vector ["#000000" "#cf6a4c" "#7ca563" "#8a9a95"
-                               "#8a9a95" "#a8799c" "#f1e694" "#c3be98"]
-      exec-path '("/usr/local/bin"
+(setq exec-path '("/usr/local/bin"
                   "/usr/bin"
                   "/bin"
                   "/usr/local/lib/node_modules/.bin/"
