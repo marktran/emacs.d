d059370301e18448ca263d57cecbc784f6151368
Mark Tran <mark.tran@gmail.com>
2012-12-14 18:25:55 -0800

    change magit branch mode evil setting

--- mqt-evil.el
+++ mqt-evil.el
@@ -32,7 +32,7 @@
 (loop for (mode . state) in '((inferior-emacs-lisp-mode      . emacs)
                               (comint-mode                   . emacs)
                               (eshell-mode                   . emacs)
-                              (magit-show-branches-mode      . emacs)
+                              (magit-branch-manager-mode     . emacs)
                               (magit-log-edit-mode           . emacs)
                               (sql-interactive-mode          . emacs))
       do (evil-set-initial-state mode state))
