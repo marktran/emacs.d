69941d1abd800042c8a9c932e7c3d3e52c879924
Mark Tran <mark.tran@gmail.com>
2012-12-25 06:09:41 -0800

    don't cycle eshell completions

--- settings/eshell.el
+++ settings/eshell.el
@@ -1,21 +1,6 @@
 (setq eshell-aliases-file "~/.emacs.d/eshell/alias"
       eshell-banner-message ""
+      eshell-cmpl-cycle-completions nil
+      eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.git\\)/\\'"
       eshell-last-dir-ring-size 10
       eshell-list-files-after-cd t)
-
-(add-hook 'eshell-mode-hook
-          (lambda ()
-            (dolist (command '("htop"
-                               "ssh"))
-              (add-to-list 'eshell-visual-commands command))
-
-            (local-set-key (kbd "C-w C-k") 'windmove-up)
-            (local-set-key (kbd "C-w C-l") 'windmove-right)
-            (local-set-key (kbd "C-w C-j") 'windmove-down)
-            (local-set-key (kbd "C-w C-h") 'windmove-left)
-
-            (local-set-key (kbd "C-w k") 'windmove-up)
-            (local-set-key (kbd "C-w l") 'windmove-right)
-            (local-set-key (kbd "C-w j") 'windmove-down)
-            (local-set-key (kbd "C-w h") 'windmove-left)))
-
