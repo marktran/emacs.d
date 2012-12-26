2bb405cd26e81f72b47358e927c0d79053af5999
Mark Tran <mark.tran@gmail.com>
2012-12-26 04:39:33 -0800

    refactor evil settings

--- settings/evil.el
+++ settings/evil.el
@@ -1,13 +1,39 @@
 (evil-mode t)
 
-(setq evil-leader/in-all-states t
+(setq evil-ex-search-vim-style-regexp t
+      evil-leader/in-all-states t
       evil-leader/leader ","
       evil-mode-line-format nil
-      evil-move-cursor-back t)
+      evil-move-cursor-back t
+      evil-search-module 'evil-search)
 (setq-default evil-shift-width 2)
 
 (define-key evil-motion-state-map evil-leader/leader evil-leader/map)
 
+(loop for (mode . state) in '((inferior-emacs-lisp-mode      . emacs)
+                              (comint-mode                   . emacs)
+                              (eshell-mode                   . emacs)
+                              (magit-branch-manager-mode     . emacs)
+                              (magit-log-edit-mode           . emacs)
+                              (sql-interactive-mode          . emacs))
+      do (evil-set-initial-state mode state))
+
+;; key bindings
+(fill-keymap evil-normal-state-map
+             "SPC"   'ace-jump-char-mode
+             "C-c +" 'evil-numbers/inc-at-pt
+             "C-c -" 'evil-numbers/dec-at-pt)
+
+(fill-keymap evil-window-map
+             "SPC" 'swap-window
+             "u"   'winner-undo
+             "C-r" 'winner-redo
+             "M-h" 'swap-with-left
+             "M-j" 'swap-with-down
+             "M-k" 'swap-with-up
+             "M-l" 'swap-with-right)
+
+;; evil leader
 (evil-leader/set-key
   "a" 'ack
   "b" 'ido-switch-buffer
@@ -27,30 +53,28 @@
   "t" 'ido-goto-symbol
   "v" 'rspec-verify)
 
-(loop for (mode . state) in '((inferior-emacs-lisp-mode      . emacs)
-                              (comint-mode                   . emacs)
-                              (eshell-mode                   . emacs)
-                              (magit-branch-manager-mode     . emacs)
-                              (magit-log-edit-mode           . emacs)
-                              (sql-interactive-mode          . emacs))
-      do (evil-set-initial-state mode state))
+;; compilation mode
+(evil-declare-key 'motion compilation-mode-map
+                  "h" 'evil-backward-char
+                  "0" 'evil-digit-argument-or-evil-beginning-of-line)
 
+;; org mode
 (evil-declare-key 'normal org-mode-map
-  (kbd "RET") 'org-open-at-point
-  "za"        'org-cycle
-  "zA"        'org-shifttab
-  "zm"        'hide-body
-  "zr"        'show-all
-  "zo"        'show-subtree
-  "zO"        'show-all
-  "zc"        'hide-subtree
-  "zC"        'org-hide-block-all
-  (kbd "M-j") 'org-shiftleft
-  (kbd "M-k") 'org-shiftright
-  (kbd "M-H") 'org-metaleft
-  (kbd "M-J") 'org-metadown
-  (kbd "M-K") 'org-metaup
-  (kbd "M-L") 'org-metaright)
+                  "za"        'org-cycle
+                  "zA"        'org-shifttab
+                  "zc"        'hide-subtree
+                  "zC"        'org-hide-block-all
+                  "zm"        'hide-body
+                  "zo"        'show-subtree
+                  "zO"        'show-all
+                  "zr"        'show-all
+                  (kbd "RET") 'org-open-at-point
+                  (kbd "M-j") 'org-shiftleft
+                  (kbd "M-k") 'org-shiftright
+                  (kbd "M-H") 'org-metaleft
+                  (kbd "M-J") 'org-metadown
+                  (kbd "M-K") 'org-metaup
+                  (kbd "M-L") 'org-metaright)
 
 (evil-declare-key 'insert org-mode-map
                   (kbd "M-j") 'org-shiftleft
@@ -59,20 +83,3 @@
                   (kbd "M-J") 'org-metadown
                   (kbd "M-K") 'org-metaup
                   (kbd "M-L") 'org-metaright)
-
-(eval-after-load 'compile
-  '(progn
-     (evil-define-key 'motion compilation-mode-map "h" 'evil-backward-char)
-     (evil-define-key 'motion compilation-mode-map "0" 'evil-digit-argument-or-evil-beginning-of-line)))
-
-(fill-keymap evil-normal-state-map
-             "SPC" 'ace-jump-char-mode)
-
-(fill-keymap evil-window-map
-             "M-h" 'swap-with-left
-             "M-j" 'swap-with-down
-             "M-k" 'swap-with-up
-             "M-l" 'swap-with-right
-             "SPC" 'swap-window
-             "u"   'winner-undo
-             "C-r" 'winner-redo)
