8d83edf7ba7ecdcad647b08071d9d38669a174a3
Mark Tran <mark.tran@gmail.com>
2013-01-31 16:39:11 -0800

    remove whitespace mode from python config

--- settings/python.el
+++ settings/python.el
@@ -1,10 +1,7 @@
 (setq py-python-command-args '("-colors" "NoColor"))
 
 (add-hook 'python-mode-hook
-          '(lambda () (whitespace-mode t)) t)
-
-(add-hook 'python-mode-hook
-  (function (lambda ()
-          (setq evil-shift-width python-indent))))
+          '(lambda ()
+             (setq evil-shift-width python-indent)))
 
 (add-hook 'python-mode-hook 'run-coding-hook)
