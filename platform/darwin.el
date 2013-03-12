a89826c0b53c59e9eff09b9de2e570e8a869a01a
Mark Tran <mark.tran@gmail.com>
2013-03-11 23:34:19 -0700

    fix indentation

--- platform/darwin.el
+++ platform/darwin.el
@@ -9,10 +9,10 @@
   (start-process (concat "open " url) nil "open" "-g" url))
 
 (defun copy-from-osx ()
-   (shell-command-to-string "pbpaste"))
+  (shell-command-to-string "pbpaste"))
 
 (defun paste-to-osx (text &optional push)
-   (let ((process-connection-type nil))
-        (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-               (process-send-string proc text)
-                     (process-send-eof proc))))
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
