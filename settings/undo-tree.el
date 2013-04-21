3b768fa8bdfd4886e0d154d24e613be9054c9d07
Mark Tran <mark.tran@gmail.com>
2013-04-20 22:47:16 -0700

    snippet to maintain region when undoing in region

--- settings/undo-tree.el
+++ settings/undo-tree.el
@@ -1,1 +1,14 @@
 (global-undo-tree-mode t)
+
+;; keep region when undoing in region
+;; http://whattheemacsd.com/my-misc.el-02.html
+(defadvice undo-tree-undo (around keep-region activate)
+  (if (use-region-p)
+      (let ((m (set-marker (make-marker) (mark)))
+            (p (set-marker (make-marker) (point))))
+        ad-do-it
+        (goto-char p)
+        (set-mark m)
+        (set-marker p nil)
+        (set-marker m nil))
+    ad-do-it))
