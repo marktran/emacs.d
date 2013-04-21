047573028274231f245026406a16d257db426b05
Mark Tran <mark.tran@gmail.com>
2013-04-21 16:06:17 -0700

    get rid of eldoc

--- settings/lisp.el
+++ settings/lisp.el
@@ -1,14 +1,6 @@
-(require 'cl)
-(require 'eldoc)
-
-(eldoc-add-command 'paredit-backward-delete
-                   'paredit-close-round)
-
 (dolist (hook '(emacs-lisp-mode-hook
                 lisp-interaction-mode-hook
                 lisp-mode-hook
                 scheme-mode-hook
                 slime-repl-mode-hook))
   (add-hook hook 'paredit-mode))
-
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
