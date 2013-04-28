3f4579211ffdbe4638e4d7480d328e5a37a3d02b
Mark Tran <mark.tran@gmail.com>
2013-04-28 02:50:00 -0700

    change yasnippet trigger to backtab

--- settings/yasnippet.el
+++ settings/yasnippet.el
@@ -1,4 +1,8 @@
-(yas-global-mode t)
 (setq yas-prompt-functions '(yas/ido-prompt)
       yas-use-menu 'abbreviate
       yas-verbosity 0)
+
+(define-key yas-minor-mode-map (kbd "<backtab>") 'yas/expand)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+
+(yas-global-mode t)
