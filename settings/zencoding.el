34a56e868e4ba5e34dd1c95b430b7a654adf9d51
Mark Tran <mark.tran@gmail.com>
2013-05-09 18:05:55 -0700

    zencoding configuration

--- settings/zencoding.el
+++ settings/zencoding.el
@@ -1,2 +1,5 @@
 (setq zencoding-indentation 2
       zencoding-preview-default nil)
+
+(define-key zencoding-mode-keymap (kbd "<backtab>") 'zencoding-expand-line)
+(define-key zencoding-mode-keymap (kbd "C-j") nil)
