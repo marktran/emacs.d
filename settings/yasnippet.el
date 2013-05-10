5b60d135bcb65eb2ed0065640fb516d3c73dfadc
Mark Tran <mark.tran@gmail.com>
2013-05-09 18:05:30 -0700

    new snippets/snippets refactor

--- settings/yasnippet.el
+++ settings/yasnippet.el
@@ -1,8 +1,7 @@
+(add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
+
 (setq yas-prompt-functions '(yas/ido-prompt)
       yas-use-menu 'abbreviate
       yas-verbosity 0)
-
-(define-key yas-minor-mode-map (kbd "<backtab>") 'yas/expand)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
 
 (yas-global-mode t)
