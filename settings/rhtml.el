052c9b098e3a801c99a595ec31d07864c96a64ac
Mark Tran <mark.tran@gmail.com>
2013-05-31 16:21:59 -0700

    defadvice for sgml-delete-tag

--- settings/rhtml.el
+++ settings/rhtml.el
@@ -1,3 +1,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
 (add-hook 'rhtml-mode-hook 'zencoding-mode)
 
+;; after deleting a tag, indent properly
+;; http://whattheemacsd.com/setup-html-mode.el-05.html
+(defadvice sgml-delete-tag (after reindent activate)
+  (indent-region (point-min) (point-max)))
