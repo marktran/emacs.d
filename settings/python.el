e693db8229dbfb8d4187c759fca6874387ee0648
Mark Tran <mark.tran@gmail.com>
2013-06-08 21:45:58 -0700

    don't set evil-shift width for python-mode

--- settings/python.el
+++ settings/python.el
@@ -1,7 +1,2 @@
 (setq py-python-command-args '("-colors" "NoColor"))
-
-(add-hook 'python-mode-hook
-          '(lambda ()
-             (setq evil-shift-width python-indent)))
-
 (add-hook 'python-mode-hook 'run-coding-hook)
