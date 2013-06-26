a058f49da91702ac79a89c5796648bf96831d8ef
Mark Tran <mark.tran@gmail.com>
2013-06-26 14:22:17 -0700

    enable golden-ratio-mode globally, diminish it

--- settings/diminish.el
+++ settings/diminish.el
@@ -2,6 +2,7 @@
 (diminish 'visual-line-mode)
 (eval-after-load "abbrev" '(diminish 'abbrev-mode))
 (eval-after-load "eldoc" '(diminish 'eldoc-mode))
+(eval-after-load "golden-ratio" '(diminish 'golden-ratio-mode))
 (eval-after-load "paredit" '(diminish 'paredit-mode))
 (eval-after-load "projectile" '(diminish 'projectile-mode))
 (eval-after-load "ruby-electric" '(diminish 'ruby-electric-mode))
--- settings/golden-ratio.el
+++ settings/golden-ratio.el
@@ -3,7 +3,11 @@
 (dolist (command '(evil-window-left
                    evil-window-up
                    evil-window-right
-                   evil-window-down))
+                   evil-window-down
+                   select-window-1
+                   select-window-2
+                   select-window-3
+                   select-window-4))
   (add-to-list 'golden-ratio-extra-commands command))
 
-
+(golden-ratio-mode)
