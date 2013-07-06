d2ffdc02b8b9c26d0b0228188ca7cb59207b644d
Mark Tran <mark.tran@gmail.com>
2013-07-06 05:20:09 -0700

    advise quit-window to kill buffer

---
+++ functions/quit-window.el
@@ -0,0 +1,6 @@
+;; http://superuser.com/questions/397806/emacs-modify-quit-window-to-delete-buffer-not-just-bury-it
+(defadvice quit-window (before quit-window-always-kill)
+  "When running `quit-window', always kill the buffer."
+  (ad-set-arg 0 t))
+
+(ad-activate 'quit-window)
