fd979e3f047e115ddd292221c3a3e612e6c52643
Mark Tran <mark.tran@gmail.com>
2013-07-06 21:21:09 -0700

    rename functions directory to definitions

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
 (load-file "~/.emacs.d/packages.el")
-(mapc 'load (directory-files "~/.emacs.d/functions" t "^[A-Za-z-]*\\.el"))
+(mapc 'load (directory-files "~/.emacs.d/definitions" t "^[A-Za-z-]*\\.el"))
 (load-file "~/.emacs.d/settings.el")
 (load-file "~/.emacs.d/bindings.el")
 (load-file "~/.emacs.d/interface.el")
