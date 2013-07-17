e3e310b7547672331c6f090635ff5a69a3eac514
Mark Tran <mark.tran@gmail.com>
2013-07-17 14:49:11 -0700

    add advise-commands macro

---
+++ definitions/advise-commands.el
@@ -0,0 +1,9 @@
+(defmacro advise-commands (advice-name commands &rest body)
+  "Apply advice named ADVICE-NAME to multiple COMMANDS.
+
+The body of the advice is in BODY."
+  `(progn
+     ,@(mapcar (lambda (command)
+                 `(defadvice ,command (before ,(intern (concat (symbol-name command) "-" advice-name)) activate)
+                    ,@body))
+               commands)))
