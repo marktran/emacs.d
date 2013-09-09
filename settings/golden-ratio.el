6a277a9487de5f7b0c4d2942d6bfe1f1f99d9905
Mark Tran <mark.tran@gmail.com>
2013-09-09 11:22:08 -0700

    uncomment golden-ratio-mode settings

--- settings/golden-ratio.el
+++ settings/golden-ratio.el
@@ -1,13 +1,13 @@
-;; (require 'golden-ratio)
+(require 'golden-ratio)
 
-;; (dolist (command '(evil-window-left
-;;                    evil-window-up
-;;                    evil-window-right
-;;                    evil-window-down
-;;                    select-window-1
-;;                    select-window-2
-;;                    select-window-3
-;;                    select-window-4))
-;;   (add-to-list 'golden-ratio-extra-commands command))
+(dolist (command '(evil-window-left
+                   evil-window-up
+                   evil-window-right
+                   evil-window-down
+                   select-window-1
+                   select-window-2
+                   select-window-3
+                   select-window-4))
+  (add-to-list 'golden-ratio-extra-commands command))
 
 ;; (golden-ratio-mode)
