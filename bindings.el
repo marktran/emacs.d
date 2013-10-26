bc6431ec9395c7401298d013d79bceda556a5afe
Mark Tran <mark.tran@gmail.com>
2013-10-26 03:49:32 -0700

    bind newline-and-indent to RET

--- bindings.el
+++ bindings.el
@@ -1,3 +1,4 @@
+(global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
 (global-set-key (kbd "M-;") 'comment-dwim-line)
 (global-set-key (kbd "M-/") 'smart-tab)
