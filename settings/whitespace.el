22a242f1aed9e41b17cbbdef3f69398e8773cfc7
Mark Tran <mark.tran@gmail.com>
2013-10-27 00:48:03 -0700

    disable show-trailing-whitespace in some modes

--- settings/whitespace.el
+++ settings/whitespace.el
@@ -4,3 +4,7 @@
 (diminish 'whitespace-cleanup-mode)
 
 (setq-default show-trailing-whitespace t)
+
+(dolist (hook '(comint-mode-hook compilation-mode-hook))
+  (add-hook hook
+            (lambda () (setq show-trailing-whitespace nil))))
