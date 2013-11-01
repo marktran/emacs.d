08796000c3dc68373b35cb473206408bd544305e
Mark Tran <mark.tran@gmail.com>
2013-10-31 22:35:56 -0700

    multiple-cursors and web-mode compatibility settings with evil-mode

--- settings/web.el
+++ settings/web.el
@@ -1,5 +1,7 @@
 (require-package 'web-mode)
 
+(evil-declare-key 'normal web-mode-map (kbd "%") 'web-mode-tag-match)
+
 (add-auto-mode 'web-mode "\\.erb\\'")
 (add-auto-mode 'web-mode "\\.html?\\'")
 (add-hook 'web-mode-hook 'emmet-mode)
