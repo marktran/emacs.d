d6102d486299e73fac6b6b0ca863adda69e671f5
Mark Tran <mark.tran@gmail.com>
2014-04-12 15:51:40 -0700

    remove org-mode package repo

--- packages.el
+++ packages.el
@@ -1,7 +1,6 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
   (package-refresh-contents))
