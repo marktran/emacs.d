75e008bf25cef381f15ec44a741fa26548185474
Mark Tran <mark.tran@gmail.com>
2014-08-28 18:03:39 -0700

    use projectile-mode instead of projectile-on

--- settings/projectile.el
+++ settings/projectile.el
@@ -3,7 +3,7 @@
   :diminish projectile-mode
   :idle (projectile-global-mode)
   :config
-  (add-hook 'enh-ruby-mode-hook 'projectile-on)
+  (add-hook 'enh-ruby-mode-hook 'projectile-mode)
   (setq projectile-completion-system 'grizzl
         projectile-enable-caching t
         projectile-tags-command "ripper-tags -R -f TAGS"))
