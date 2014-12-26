832b034b89a352a6a7482d8920d3501b714b3184
Mark Tran <mark.tran@gmail.com>
2014-12-25 21:39:02 -0500

    add helm-projectile and ensure "t"

--- settings/projectile.el
+++ settings/projectile.el
@@ -1,9 +1,13 @@
 (use-package projectile
-  :ensure projectile
+  :ensure t
   :diminish projectile-mode
   :idle (projectile-global-mode)
   :config
   (add-hook 'enh-ruby-mode-hook 'projectile-mode)
   (setq projectile-completion-system 'grizzl
         projectile-enable-caching t
+        projectile-ignored-projects '("emacs.d" "color-theme-ujelly")
         projectile-tags-command "ripper-tags -R -f TAGS"))
+
+(use-package helm-projectile
+  :ensure t)
