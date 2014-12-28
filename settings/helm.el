9681f311c749c7d97f2f0c005f72e76ebbc4bb6a
Mark Tran <mark.tran@gmail.com>
2014-12-28 01:03:10 -0500

    enable fuzzy matching for helm-M-x

--- settings/helm.el
+++ settings/helm.el
@@ -3,4 +3,5 @@
 (setq helm-ff-transformer-show-only-basename nil
       helm-idle-delay 0.1
       helm-input-idle-delay 0.1
-      helm-ls-git-show-abs-or-relative 'relative)
+      helm-ls-git-show-abs-or-relative 'relative
+      helm-M-x-fuzzy-match t)
