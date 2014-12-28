f3d00285aa674ba856fd6062b803148389c0737e
Mark Tran <mark.tran@gmail.com>
2014-12-28 02:00:11 -0500

    wrap helm candidates

--- settings/helm.el
+++ settings/helm.el
@@ -4,4 +4,5 @@
       helm-idle-delay 0.1
       helm-input-idle-delay 0.1
       helm-ls-git-show-abs-or-relative 'relative
-      helm-M-x-fuzzy-match t)
+      helm-M-x-fuzzy-match t
+      helm-move-to-line-cycle-in-source t)
