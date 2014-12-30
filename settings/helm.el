9b83a0cae6b432c94586666a0713862f9b6a3b11
Mark Tran <mark.tran@gmail.com>
2014-12-30 17:13:36 -0500

    move helm to use-package

--- settings/helm.el
+++ settings/helm.el
@@ -1,8 +1,9 @@
-(require-package 'helm)
-
-(setq helm-ff-transformer-show-only-basename nil
-      helm-idle-delay 0.1
-      helm-input-idle-delay 0.1
-      helm-ls-git-show-abs-or-relative 'relative
-      helm-M-x-fuzzy-match t
-      helm-move-to-line-cycle-in-source t)
+(use-package helm
+  :ensure t
+  :config
+  (setq helm-ff-transformer-show-only-basename nil
+        helm-idle-delay 0.1
+        helm-input-idle-delay 0.1
+        helm-ls-git-show-abs-or-relative 'relative
+        helm-M-x-fuzzy-match t
+        helm-move-to-line-cycle-in-source t))
