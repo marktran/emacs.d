8c6745e9bf1e5faa7484bfbab7e6131caae1e86f
Mark Tran <mark.tran@gmail.com>
2015-07-03 02:16:02 -0400

    only use popwin for *Help* buffers

--- settings/popwin.el
+++ settings/popwin.el
@@ -1,4 +1,9 @@
-(require-package 'popwin)
+(use-package popwin
+  :ensure t
 
-(require 'popwin)
-(popwin-mode t)
+  :init
+  (setq popwin:special-display-config nil)
+
+  :config
+  (add-to-list 'popwin:special-display-config '("*Help*" :width 80 :position right))
+  (popwin-mode t))
