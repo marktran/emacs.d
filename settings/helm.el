ca8664d63488304a525c6d0dafa49b368f6693e2
Mark Tran <mark.tran@gmail.com>
2015-07-04 18:10:48 -0400

    add helm-swoop

--- settings/helm.el
+++ settings/helm.el
@@ -1,5 +1,6 @@
 (use-package helm
   :ensure t
+
   :config
   (setq helm-ff-transformer-show-only-basename nil
         helm-idle-delay 0.1
@@ -7,3 +8,10 @@
         helm-ls-git-show-abs-or-relative 'relative
         helm-M-x-fuzzy-match t
         helm-move-to-line-cycle-in-source t))
+
+(use-package helm-swoop
+  :defer t
+
+  :init
+  (setq helm-swoop-pre-input-function (lambda () "")
+        helm-swoop-split-with-multiple-windows t))
