e1786c103d443c935013ac39d8a5f37408afec3c
Mark Tran <mark.tran@gmail.com>
2015-07-04 23:35:04 -0400

    defer loading emmet-mode

--- settings/emmet.el
+++ settings/emmet.el
@@ -1,6 +1,8 @@
 (use-package emmet-mode
-  :ensure emmet-mode
-  :bind (("<backtab>" . emmet-expand-line))
+  :defer t
+
+  :bind ("<backtab>" . emmet-expand-line)
+
   :config
   (setq emmet-indentation 2
         emmet-preview-default nil))
