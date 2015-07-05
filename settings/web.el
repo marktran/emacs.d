91916fde70fa54099971c9085fef3b422adbac7d
Mark Tran <mark.tran@gmail.com>
2015-07-04 23:35:47 -0400

    move web-mode configuration to use-package

--- settings/web.el
+++ settings/web.el
@@ -1,10 +1,13 @@
-(require-package 'web-mode)
+(use-package web-mode
+  :defer t
+  :mode
+  (("\\.erb\\'" . web-mode)
+   ("\\.html?\\'" . web-mode))
 
-(setq web-mode-code-indent-offset 2
-      web-mode-markup-indent-offset 2)
+  :config
+  (setq web-mode-code-indent-offset 2
+        web-mode-markup-indent-offset 2)
 
-(evil-declare-key 'normal web-mode-map (kbd "%") 'web-mode-tag-match)
+  (evil-declare-key 'normal web-mode-map (kbd "%") 'web-mode-tag-match)
 
-(add-auto-mode 'web-mode "\\.erb\\'")
-(add-auto-mode 'web-mode "\\.html?\\'")
-(add-hook 'web-mode-hook 'emmet-mode)
+  (add-hook 'web-mode-hook 'emmet-mode))
