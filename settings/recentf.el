1ac4a395be152de94c5a1ad1b6aed35841ee1f92
Mark Tran <mark.tran@gmail.com>
2015-07-05 00:52:51 -0400

    exclude package directory from recentf

--- settings/recentf.el
+++ settings/recentf.el
@@ -2,7 +2,9 @@
   :defer t
 
   :config
+  (add-to-list 'recentf-exclude (expand-file-name package-user-dir))
   (add-to-list 'recentf-exclude "TAGS")
+
   (setq recentf-auto-cleanup 'never
         recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list)
         recentf-max-saved-items 100))
