5dc1beff9d95b844210a7c4501f170a3e6f5c62a
Mark Tran <mark.tran@gmail.com>
2015-07-04 23:55:16 -0400

    move paredit configuration to use-package

--- settings/lisp.el
+++ settings/lisp.el
@@ -1,8 +1,12 @@
-(require-package 'paredit)
+(use-package paredit
+  :defer t
 
-(dolist (hook '(emacs-lisp-mode-hook
-                lisp-interaction-mode-hook
-                lisp-mode-hook
-                scheme-mode-hook
-                slime-repl-mode-hook))
-  (add-hook hook 'paredit-mode))
+  :config
+  (dolist (hook '(emacs-lisp-mode-hook
+                  lisp-interaction-mode-hook
+                  lisp-mode-hook
+                  scheme-mode-hook
+                  slime-repl-mode-hook))
+    (add-hook hook 'paredit-mode)))
+
+
