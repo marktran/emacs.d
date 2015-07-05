a80025004551ebcc74ac18ba20a61a52b7bfd0fd
Mark Tran <mark.tran@gmail.com>
2015-07-04 21:22:42 -0400

    move yasnippet to use-package

--- settings/yasnippet.el
+++ settings/yasnippet.el
@@ -1,9 +1,11 @@
-(require-package 'yasnippet)
+(use-package yasnippet
+  :defer t
 
-(add-auto-mode 'snippet-mode "\\.yasnippet\\'")
+  :mode ("\\.yasnippet\\'" . snippet-mode)
 
-(setq yas-prompt-functions '(yas/ido-prompt)
-      yas-use-menu 'abbreviate
-      yas-verbosity 0)
+  :config
+  (setq yas-prompt-functions '(yas/ido-prompt)
+        yas-use-menu 'abbreviate
+        yas-verbosity 0)
 
-(yas-global-mode t)
+  (yas-global-mode t))
