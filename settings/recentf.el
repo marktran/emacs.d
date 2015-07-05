638a5834edd6fc32ea74d95ef06d16aa52a6f6d8
Mark Tran <mark.tran@gmail.com>
2015-07-05 01:42:03 -0400

    move to config block

--- settings/recentf.el
+++ settings/recentf.el
@@ -1,11 +1,10 @@
 (use-package recentf
   :defer t
 
-  :init
+  :config
   (add-to-list 'recentf-exclude (expand-file-name package-user-dir))
   (add-to-list 'recentf-exclude "TAGS")
 
-  :config
   (setq recentf-auto-cleanup 'never
         recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list)
         recentf-max-saved-items 100))
