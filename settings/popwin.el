619af1b6ccbebf9f668ecf87768c75e696008712
Mark Tran <mark.tran@gmail.com>
2015-07-04 22:55:50 -0400

    put rspec-compilation into popwin

--- settings/popwin.el
+++ settings/popwin.el
@@ -9,5 +9,7 @@
                '("*Help*" :height 0.4 :dedicated t :stick t :position bottom))
   (add-to-list 'popwin:special-display-config
                '("^\*helm.+\*$" :height 0.4 :regexp t))
+  (add-to-list 'popwin:special-display-config
+               '("*rspec-compilation*" :height 0.3 :dedicated t :stick t :noselect t :position bottom))
 
   (popwin-mode t))
