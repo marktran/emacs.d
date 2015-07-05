e0a7869f67f1fe030906c380b97ed0079ab71ec8
Mark Tran <mark.tran@gmail.com>
2015-07-04 23:16:08 -0400

    move recentf configuration to use-package

--- settings/recentf.el
+++ settings/recentf.el
@@ -1,3 +1,8 @@
-(recentf-mode t)
-(setq recentf-max-saved-items 100)
-(add-to-list 'recentf-exclude "TAGS")
+(use-package recentf
+  :defer t
+
+  :config
+  (add-to-list 'recentf-exclude "TAGS")
+  (setq recentf-auto-cleanup 'never
+        recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list)
+        recentf-max-saved-items 100))
