ad208e478319fced2c65767ca4b17c172855c759
Mark Tran <mark.tran@gmail.com>
2015-07-07 21:48:33 -0400

    enable fuzzy matching in helm

--- settings/helm.el
+++ settings/helm.el
@@ -2,12 +2,14 @@
   :ensure t
 
   :config
-  (setq helm-ff-transformer-show-only-basename nil
+  (setq helm-buffers-fuzzy-matching t
+        helm-ff-transformer-show-only-basename nil
         helm-idle-delay 0.1
         helm-input-idle-delay 0.1
         helm-ls-git-show-abs-or-relative 'relative
         helm-M-x-fuzzy-match t
-        helm-move-to-line-cycle-in-source t))
+        helm-move-to-line-cycle-in-source t
+        helm-recentf-fuzzy-match t))
 
 (use-package helm-ag :ensure t)
 
