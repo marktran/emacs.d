e176a49deac899d2bdcb8351fe14bf218515250b
Mark Tran <mark.tran@gmail.com>
2015-07-11 04:15:10 -0400

    don't show trailing whitespace

--- settings/whitespace.el
+++ settings/whitespace.el
@@ -3,12 +3,5 @@
   :diminish whitespace-cleanup-mode
 
   :config
-  (setq-default show-trailing-whitespace t)
-  (global-whitespace-cleanup-mode t)
-
-  (dolist (hook '(comint-mode-hook
-                  compilation-mode-hook
-                  eshell-mode-hook
-                  help-mode-hook
-                  vc-annotate-mode-hook))
-    (add-hook hook (lambda () (setq show-trailing-whitespace nil)))))
+  (setq-default show-trailing-whitespace nil)
+  (global-whitespace-cleanup-mode t))
