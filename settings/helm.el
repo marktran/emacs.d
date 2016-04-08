c09546b6f67c10310cea797bf551db7068a35a0c
Mark Tran <mark.tran@gmail.com>
2016-04-09 00:01:26 +0700

    add helm customizations

--- settings/helm.el
+++ settings/helm.el
@@ -3,13 +3,15 @@
 
   :config
   (setq helm-buffers-fuzzy-matching t
+        helm-display-header-line nil
         helm-ff-transformer-show-only-basename nil
         helm-idle-delay 0.1
         helm-input-idle-delay 0.1
         helm-ls-git-show-abs-or-relative 'relative
         helm-M-x-fuzzy-match t
         helm-move-to-line-cycle-in-source t
-        helm-recentf-fuzzy-match t))
+        helm-recentf-fuzzy-match t
+        helm-split-window-in-side-p t))
 
 (use-package helm-ag
   :ensure t
