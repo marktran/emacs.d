6aee86af6e38a431c6267e4612f34eb7296cc5e1
Mark Tran <mark.tran@gmail.com>
2016-04-11 00:31:23 +0700

    remove indent-region-or-buffer

--- bindings.el
+++ bindings.el
@@ -1,5 +1,4 @@
 (global-set-key (kbd "RET") 'newline-and-indent)
-(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
 (global-set-key (kbd "M-;") 'comment-dwim-line)
 (global-set-key (kbd "M-/") 'smart-tab)
 (global-set-key (kbd "M-x") 'helm-M-x)
