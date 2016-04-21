cea29b745aa0d555d7a6eb945b1548be0edbc948
Mark Tran <mark.tran@gmail.com>
2016-04-21 14:47:58 +0700

    move ido into use-package block

--- settings/ido.el
+++ settings/ido.el
@@ -1,37 +1,39 @@
-(ido-mode t)
+(use-package ido
+  :init
+  (setq ido-auto-merge-work-directories-length 1
+        ido-create-new-buffer 'always
+        ido-enable-flex-matching t
+        ido-ignore-buffers `("\\` "
+                             "^\\*Buffer List\\*"
+                             "^\\*Compile-Log\\*"
+                             "^\\*Completions\\*"
+                             "^\\*Helm"
+                             "^\\*Help\\*"
+                             "^\\*Ido"
+                             "^\\*Messages\\*"
+                             "^\\*RE-Builder\\*"
+                             "^\\*Shell Command Output\\*"
+                             "^\\*Warnings\\*"
+                             "^\\*XML Validation Header\\*"
+                             "^\\*growl\\*"
+                             "^\\*helm"
+                             "^\\*magit"
+                             "TAGS"
+                             (lambda (name)
+                               (save-excursion
+                                 (equal major-mode 'dired-mode))))
+        ido-use-filename-at-point nil
+        ido-use-virtual-buffers nil)
 
-(setq ido-auto-merge-work-directories-length 1
-      ido-create-new-buffer 'always
-      ido-enable-flex-matching t
-      ido-ignore-buffers `("\\` "
-                           "^\\*Buffer List\\*"
-                           "^\\*Compile-Log\\*"
-                           "^\\*Completions\\*"
-                           "^\\*Helm"
-                           "^\\*Help\\*"
-                           "^\\*Ido"
-                           "^\\*Messages\\*"
-                           "^\\*RE-Builder\\*"
-                           "^\\*Shell Command Output\\*"
-                           "^\\*Warnings\\*"
-                           "^\\*XML Validation Header\\*"
-                           "^\\*growl\\*"
-                           "^\\*helm"
-                           "^\\*magit"
-                           "TAGS"
-                           (lambda (name)
-                             (save-excursion
-                               (equal major-mode 'dired-mode))))
-      ido-use-filename-at-point nil
-      ido-use-virtual-buffers nil)
+  :config
+  (add-to-list 'ido-ignore-files "\\.DS_Store")
+  (add-to-list 'ido-ignore-files "Icon$")
 
-(add-to-list 'ido-ignore-files "\\.DS_Store")
-(add-to-list 'ido-ignore-files "Icon$")
+  (add-hook 'ido-setup-hook
+            (gen-fill-keymap-hook ido-completion-map
+                                  "C-h" 'ido-prev-match
+                                  "C-l" 'ido-next-match))
 
-(add-hook 'ido-setup-hook
-          (gen-fill-keymap-hook ido-completion-map
-                                "C-h" 'ido-prev-match
-                                "C-l" 'ido-next-match))
+  (ido-mode t))
 
-(use-package ido-occasional
-  :ensure t)
+(use-package ido-occasional)
