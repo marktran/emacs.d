325a019b0e6536663e42645432a1e49a6f580f54
Mark Tran <mark.tran@gmail.com>
2016-05-20 11:55:42 +0700

    remove delay; autocomplete single entries

--- settings/helm.el
+++ settings/helm.el
@@ -9,9 +9,8 @@
   :config
   (setq helm-buffers-fuzzy-matching t
         helm-display-header-line nil
+        helm-ff-auto-update-initial-value t
         helm-ff-transformer-show-only-basename nil
-        helm-idle-delay 0.1
-        helm-input-idle-delay 0.1
         helm-ls-git-show-abs-or-relative 'relative
         helm-M-x-fuzzy-match t
         helm-move-to-line-cycle-in-source t
