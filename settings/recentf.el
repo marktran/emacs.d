7b8fee3d04fa5218e11c1f8d8d56fcda735e0c0d
Mark Tran <mark.tran@gmail.com>
2016-05-20 12:22:40 +0700

    add more exclusions to recentf

--- settings/recentf.el
+++ settings/recentf.el
@@ -2,8 +2,10 @@
   :defer t
   :config
   (add-to-list 'recentf-exclude (expand-file-name package-user-dir))
-  (add-to-list 'recentf-exclude "TAGS")
+  (add-to-list 'recentf-exclude ".bookmarks")
   (add-to-list 'recentf-exclude "ido.last")
+  (add-to-list 'recentf-exclude "recentf")
+  (add-to-list 'recentf-exclude "TAGS")
 
   (setq recentf-auto-cleanup 'never
         recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list)
