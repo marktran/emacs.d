1ba1b88fb7f202de17b055035b2005b27dbafe96
Mark Tran <mark.tran@gmail.com>
2016-05-30 18:28:00 +0700

    refactor recentf-exclude

--- settings/recentf.el
+++ settings/recentf.el
@@ -1,12 +1,16 @@
 (use-package recentf
-  :defer t
-  :config
-  (add-to-list 'recentf-exclude (expand-file-name package-user-dir))
-  (add-to-list 'recentf-exclude ".bookmarks")
-  (add-to-list 'recentf-exclude "ido.last")
-  (add-to-list 'recentf-exclude "recentf")
-  (add-to-list 'recentf-exclude "TAGS")
+  :init
+  (recentf-mode)
 
+  :config
   (setq recentf-auto-cleanup 'never
         recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list)
+
+        recentf-exclude (list (expand-file-name package-user-dir)
+                              #'ignoramus-boring-p
+                              ".bookmarks"
+                              "ido.last"
+                              "recentf"
+                              "TAGS")
+
         recentf-max-saved-items 150))
