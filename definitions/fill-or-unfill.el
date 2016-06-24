52767ebebfe7d66851ec17f80f23059b98756a3a
Mark Tran <mark.tran@gmail.com>
2016-06-25 06:37:47 +0700

    add fill-or-unfill definition

---
+++ definitions/fill-or-unfill.el
@@ -0,0 +1,10 @@
+;; http://endlessparentheses.com/fill-and-unfill-paragraphs-with-a-single-key.html
+(defun fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
