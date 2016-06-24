2c8c23c13917d041465143a57ad8e191b782afdb
Mark Tran <mark.tran@gmail.com>
2016-06-25 06:38:05 +0700

    load bindings earlier

--- init.el
+++ init.el
@@ -5,8 +5,8 @@
 
 (load-file "~/.emacs.d/packages.el")
 (mapc 'load (directory-files "~/.emacs.d/definitions" t "^[A-Za-z-]*\\.el"))
-(load-file "~/.emacs.d/settings.el")
 (load-file "~/.emacs.d/bindings.el")
+(load-file "~/.emacs.d/settings.el")
 (load-file "~/.emacs.d/interface.el")
 (load-file "~/.emacs.d/platform.el")
 
