3d32e220ac133c908c0e0c88753655e1636356e6
Mark Tran <mark.tran@gmail.com>
2016-06-26 13:38:17 +0700

    add workaround for eshell-mode-map being buffer local

--- settings/eshell.el
+++ settings/eshell.el
@@ -1,25 +1,6 @@
 (use-package eshell
   :commands eshell
 
-  :general
-  (:keymaps 'eshell-mode-map
-    "C-d" 'bury-buffer)
-
-  (:keymaps 'eshell-mode-map
-   :prefix "C-w"
-   "=" 'balance-windows
-   "h" 'windmove-left
-   "C-h" 'windmove-left
-   "l" 'windmove-right
-   "C-l" 'windmove-right
-   "j" 'windmove-down
-   "C-j" 'windmove-down
-   "k" 'windmove-up
-   "C-k" 'windmove-up
-   "C-o" 'delete-other-windows
-   "c" 'delete-window
-   "u" 'winner-undo)
-
   :config
   (setq eshell-aliases-file "~/.emacs.d/eshell/alias"
         eshell-banner-message ""
@@ -39,4 +20,26 @@
         eshell-prompt-regexp "^[^ ]* [^ ]* "
         esehll-review-quick-commands t
         eshell-smart-space-goes-to-end t
-        eshell-where-to-jump 'begin))
+        eshell-where-to-jump 'begin)
+
+  ;; https://github.com/noctuid/general.el/issues/32
+  (defun custom/eshell-keybindings ()
+    (progn
+      (general-define-key :keymaps 'eshell-mode-map
+        "C-d" 'bury-buffer)
+
+      (general-define-key :keymaps 'eshell-mode-map :prefix "C-w"
+        "=" 'balance-windows
+        "h" 'windmove-left
+        "C-h" 'windmove-left
+        "l" 'windmove-right
+        "C-l" 'windmove-right
+        "j" 'windmove-down
+        "C-j" 'windmove-down
+        "k" 'windmove-up
+        "C-k" 'windmove-up
+        "C-o" 'delete-other-windows
+        "c" 'delete-window
+        "u" 'winner-undo)))
+
+  (add-hook 'eshell-mode-hook #'custom/eshell-keybindings))
