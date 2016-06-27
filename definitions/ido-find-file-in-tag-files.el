42df46aaf5c6d4fa831eb777f6a5facf0712aeb1
Mark Tran <mark.tran@gmail.com>
2016-06-27 21:27:35 +0700

    delete unused functions

--- definitions/ido-find-file-in-tag-files.el
+++
@@ -1,8 +0,0 @@
-;; http://www.emacswiki.org/emacs/TextMate
-(defun ido-find-file-in-tag-files ()
-  (interactive)
-  (save-excursion
-    (let ((enable-recursive-minibuffers t)) (visit-tags-table-buffer))
-    (find-file (expand-file-name
-                (ido-completing-read "Project file: "
-                                     (tags-table-files) nil t)))))
