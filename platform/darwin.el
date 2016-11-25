8e28d82591ab2fbcd3bff4bb665725b82e6160e7
Mark Tran <mark.tran@gmail.com>
2016-11-25 19:23:41 +0700

    enable save-interprogram-paste-before-kill

--- platform/darwin.el
+++ platform/darwin.el
@@ -2,17 +2,9 @@
       delete-by-moving-to-trash t
       dired-use-ls-dired t
       insert-directory-program "gls"
-      mouse-wheel-scroll-amount '(0.01))
+      mouse-wheel-scroll-amount '(0.01)
+      save-interprogram-paste-before-kill t)
 
 (defun browse-url-default-macosx-browser (url &optional new-window)
   (interactive (browse-url-interactive-arg "URL: "))
   (start-process (concat "open " url) nil "open" url))
-
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
-
-(defun paste-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
