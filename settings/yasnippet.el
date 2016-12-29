664e8df4f008462f5df57a4f66fdd237c082288b
Mark Tran <mark.tran@gmail.com>
2016-12-29 16:40:15 +0700

    disable yasnippet globally

--- settings/yasnippet.el
+++ settings/yasnippet.el
@@ -2,9 +2,6 @@
   :diminish yas-minor-mode
   :mode ("\\.yasnippet\\'" . snippet-mode)
 
-  :init
-  (yas-global-mode 1)
-
   :config
   (setq yas-prompt-functions 'yas-completing-prompt
         yas-use-menu 'abbreviate
