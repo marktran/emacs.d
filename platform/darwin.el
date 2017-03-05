459f5979ed0b0031a0577b344b5bdba488a49419
Mark Tran <mark.tran@gmail.com>
2017-03-05 20:56:08 +0700

    disable xterm-mouse-mode on darwin

--- platform/darwin.el
+++ platform/darwin.el
@@ -5,6 +5,8 @@
       mouse-wheel-scroll-amount '(0.01)
       save-interprogram-paste-before-kill t)
 
+(xterm-mouse-mode -1)
+
 (defun browse-url-default-macosx-browser (url &optional new-window)
   (interactive (browse-url-interactive-arg "URL: "))
   (start-process (concat "open " url) nil "open" url))
