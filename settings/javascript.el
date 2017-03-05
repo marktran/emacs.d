97c532b7c6d6bfeb18d1a77e50d2a2a98470bd54
Mark Tran <mark.tran@gmail.com>
2017-03-05 21:01:04 +0700

    rename file; add js2-mode

---
+++ settings/javascript.el
@@ -0,0 +1,13 @@
+(use-package js-mode
+  :ensure nil
+  :config
+  (setq js-indent-level 2))
+
+(use-package js2-mode
+  :mode ("\\.js\\'" . js2-mode)
+
+  :init
+  (rename-modeline "js2-mode" js2-mode "Javascript")
+
+  :config
+  (setq js2-basic-offset 2))
--- settings/js.el
+++
@@ -1,5 +0,0 @@
-(use-package js-mode
-  :ensure nil
-  :mode "\\.js"
-  :init
-  (setq js-indent-level 2))
