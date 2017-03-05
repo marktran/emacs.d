ae910008c1ec70f6ee3fa97c14608ffeda3deadb
Mark Tran <mark.tran@gmail.com>
2017-03-05 21:00:32 +0700

    add .vue files in web-mode; configuration indentation and javascript comments

--- settings/web.el
+++ settings/web.el
@@ -1,11 +1,19 @@
 (use-package web-mode
   :mode
   (("\\.erb\\'" . web-mode)
-   ("\\.html?\\'" . web-mode))
+   ("\\.html?\\'" . web-mode)
+   ("\\.vue\\'" . web-mode))
 
   :config
+  (setq-default web-mode-comment-formats
+                '(("javascript" . "//")))
+
   (setq web-mode-code-indent-offset 2
-        web-mode-markup-indent-offset 2)
+        web-mode-css-indent-offset 2
+        web-mode-enable-css-colorization t
+        web-mode-markup-indent-offset 2
+        web-mode-script-padding 2
+        web-mode-style-padding 2)
 
   (evil-declare-key 'normal web-mode-map (kbd "%") 'web-mode-tag-match)
 
