25585d0ff5471dee2bfbad2484c017b59d272297
Mark Tran <mark.tran@gmail.com>
2017-05-08 10:27:28 +0700

    change position of requires

--- init.el
+++ init.el
@@ -10,15 +10,15 @@
 (require 'cl)
 (require 'use-package)
 
+(load "~/.emacs.d/local.el" 'noerror)
+(load "~/.emacs.d/custom.el" 'noerror)
+
 (load-file "~/.emacs.d/packages.el")
 (mapc 'load (directory-files "~/.emacs.d/definitions" t "^[A-Za-z-]*\\.el"))
 (load-file "~/.emacs.d/bindings.el")
 (load-file "~/.emacs.d/settings.el")
 (load-file "~/.emacs.d/interface.el")
 (load-file "~/.emacs.d/platform.el")
-
-(load "~/.emacs.d/local.el" 'noerror)
-(load "~/.emacs.d/custom.el" 'noerror)
 
 (setq user-full-name "Mark Tran"
       user-mail-address "mark.tran@gmail.com")
