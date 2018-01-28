d4cbe5fd720b4bdb6c9b9e5415ef574a9893b1d9
Mark Tran <mark.tran@gmail.com>
2018-01-29 05:54:12 +0700

    hide .gitkeep in dired

--- settings/dired.el
+++ settings/dired.el
@@ -10,6 +10,6 @@
   (use-package dired+ :config (diredp-toggle-find-file-reuse-dir t))
 
   (setq dired-listing-switches "-alh"
-        dired-omit-files "^\\.?#\\|^\\.$\\|^\\.DS_Store$"
+        dired-omit-files "^\\.?#\\|^\\.$\\|\\.DS_Store$\\|\\.gitkeep$"
         dired-recursive-copies 'always
         dired-recursive-deletes 'always))
