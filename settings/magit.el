8f2eb0ded13767881230d875bdf9fca9accd7371
Mark Tran <mark.tran@gmail.com>
2018-04-28 18:29:02 +0800

    use :custom keyword

--- settings/magit.el
+++ settings/magit.el
@@ -1,20 +1,14 @@
 (use-package magit
-  :config
-  (use-package evil-magit)
+  :custom
+  (magit-auto-revert-mode nil)
+  (magit-completing-read-function 'ivy-completing-read)
+  (magit-prefer-remote-upstream t)
+  (magit-push-always-verify nil)
+  (magit-remote-ref-format 'remote-slash-name)
+  (magit-revert-buffers 'silent)
+  (vc-handled-backends nil)
 
-  (general-define-key :prefix "SPC"
-   "g"  '(:ignore t :which-key "Magit")
-   "g b" '(magit-blame :which-key "Blame")
-   "g l" '(magit-log-current :which-key "Log")
-   "g L" '(magit-log-buffer-file :which-key "Log [for file]")
-   "g s" '(magit-status :which-key "Status"))
-
-
-  (setq magit-auto-revert-mode nil
-        magit-branch-arguments (remove "--track" magit-branch-arguments)
-        magit-completing-read-function 'ivy-completing-read
-
-        magit-display-buffer-function
+  (magit-display-buffer-function
         (lambda (buffer)
           (if (or
                magit-display-buffer-noselect
@@ -29,12 +23,17 @@
             (delete-other-windows)
             (set-window-dedicated-p nil nil)
             (set-window-buffer nil buffer)
-            (get-buffer-window buffer)))
+            (get-buffer-window buffer))))
 
-        magit-push-always-verify nil
-        magit-remote-ref-format 'remote-slash-name
-        magit-revert-buffers 'silent
-        vc-handled-backends nil)
+  :config
+  (setq magit-branch-arguments (remove "--track" magit-branch-arguments))
+
+  (general-define-key :prefix "SPC"
+   "g"  '(:ignore t :which-key "Magit")
+   "g b" '(magit-blame :which-key "Blame")
+   "g l" '(magit-log-current :which-key "Log")
+   "g L" '(magit-log-buffer-file :which-key "Log [for file]")
+   "g s" '(magit-status :which-key "Status"))
 
   (evil-set-initial-state 'git-commit-mode 'emacs)
   (evil-set-initial-state 'magit-popup-mode 'emacs)
@@ -42,6 +41,8 @@
   (evil-set-initial-state 'magit-refs-mode 'emacs)
   (evil-set-initial-state 'magit-revision-mode 'emacs)
 
-  (evil-make-overriding-map magit-blame-mode-map 'normal)
   (add-to-list 'magit-no-confirm 'stage-all-changes)
   (add-hook 'magit-blame-mode-hook 'evil-normalize-keymaps))
+
+(use-package evil-magit
+  :after magit)
