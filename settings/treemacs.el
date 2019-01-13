dfb7d6f237feac596c29a2c8bb0dc7798ff43b20
Mark Tran <mark.tran@gmail.com>
2019-01-13 01:28:47 -0500

    treemacs updates

--- settings/treemacs.el
+++ settings/treemacs.el
@@ -1,9 +1,19 @@
 (use-package treemacs
-  :config
-  (use-package treemacs-evil)
-  (use-package treemacs-projectile)
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
 
-  (setq treemacs-follow-after-init t
+  :config
+  (setq treemacs-file-event-delay 5000
+        treemacs-file-follow-delay 0.2
+        treemacs-follow-after-init t
         treemacs-git-integration t)
 
+  (treemacs-filewatch-mode 1)
   (treemacs-follow-mode 1))
+
+(use-package treemacs-evil
+  :after treemacs evil)
+
+(use-package treemacs-projectile
+  :after treemacs projectile)
