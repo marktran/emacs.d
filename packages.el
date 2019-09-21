31b3dad461f205bdb97c9c8690713c8a704288f7
Mark Tran <mark.tran@gmail.com>
2019-09-21 11:30:32 -0400

    package.el upgrades

--- packages.el
+++ packages.el
@@ -1,5 +1,3 @@
-(require 'package)
-
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
 (unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
--- platform/darwin.el
+++ platform/darwin.el
@@ -5,14 +5,10 @@
       mouse-wheel-scroll-amount '(0.01)
       save-interprogram-paste-before-kill t)
 
-(if (eq window-system 'mac)
-    (set-face-attribute 'default nil :family "Operator Mono Medium" :height 130)
-  (set-frame-font "Operator Mono Medium 13"))
-
-(when (eq window-system 'mac)
-  (toggle-frame-fullscreen))
-
 (xterm-mouse-mode -1)
+
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
 
 (defun browse-url-default-macosx-browser (url &optional new-window)
   (interactive (browse-url-interactive-arg "URL: "))
