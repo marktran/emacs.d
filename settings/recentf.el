71a27f088df4ac3e7f1629d4ec076c31f0cdce1e
Mark Tran <mark.tran@gmail.com>
2024-11-09 16:28:15 -0800

    refactor recentf configuration

--- settings/recentf.el
+++ settings/recentf.el
@@ -1,15 +1,14 @@
 (use-package recentf
-  :init
-  (recentf-mode)
-
-  :config
-  (setq recentf-auto-cleanup 'never
-        recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list)
-
-        recentf-exclude (list (expand-file-name package-user-dir)
+  :custom
+  (recentf-auto-cleanup 'never)
+  (recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list))
+  (recentf-exclude (list (expand-file-name package-user-dir)
                               #'ignoramus-boring-p
                               ".bookmarks"
                               "recentf"
-                              "TAGS")
+                              "TAGS"))
+  (recentf-keep '(file-remote-p file-readable-p))
+  (recentf-max-saved-items 150)
 
-        recentf-max-saved-items 150))
+  :init
+  (recentf-mode))
