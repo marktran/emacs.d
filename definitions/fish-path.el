8429c8ffd4ea5beebcebaeb53cc8d47afbe985bb
Mark Tran <mark.tran@gmail.com>
2024-11-10 01:09:48 -0800

    rewrite without depending on cl/cl-lib

--- definitions/fish-path.el
+++ definitions/fish-path.el
@@ -5,7 +5,7 @@ parent directories with their initial characters to try to get the character
 length of PATH (sans directory slashes) down to MAX-LEN."
   (let* ((components (split-string (abbreviate-file-name path) "/"))
          (len (+ (1- (length components))
-                 (reduce '+ components :key 'length)))
+                 (apply '+ (mapcar #'length components))))  ;; Compute total length manually
          (str ""))
     (while (and (> len max-len)
                 (cdr components))
@@ -19,6 +19,9 @@ length of PATH (sans directory slashes) down to MAX-LEN."
                                    (concat (substring (car components) 0 2)
                                            "/")
                                  (string (elt (car components) 0) ?/)))))
-            len (- len (1- (length (car components))))
+            len (- len (1+ (length (car components))))  ;; Adjust length manually
             components (cdr components)))
-    (concat str (reduce (lambda (a b) (concat a "/" b)) components))))
+    ;; Concatenate remaining components manually
+    (dolist (comp components)
+      (setq str (concat str "/" comp)))
+    str))
