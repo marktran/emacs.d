1f94c43e09452da53522520c1c3123cd57ff87e9
Mark Tran <mark.tran@gmail.com>
2024-11-10 00:52:22 -0800

    merge typescript configuration into javascript file

--- settings/javascript.el
+++ settings/javascript.el
@@ -9,9 +9,33 @@
   :config
   (setq js2-basic-offset 2))
 
-(use-package add-node-modules-path)
+(use-package typescript-ts-mode
+  :custom
+  (typescript-ts-mode-indent-offset 2)
+
+  :general
+  (:keymaps 'typescript-ts-mode-map
+   :states 'normal
+   :prefix "SPC"
+   "b f" '(prettier-js :which-key "Format")))
+
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all))
+
+(use-package add-node-modules-path
+  :hook
+  (typescript-ts-mode . add-node-modules-path))
 
 (use-package prettier-js
   :after js2-mode
+  :diminish ""
+
   :config
-  (setq prettier-args '("--no-semi" "all")))
+  (setq prettier-args '("--no-semi" "all"))
+
+  :hook
+  (typescript-ts-mode . prettier-js-mode))
