da8f95dadc57bd49a5cb54a08d460995bd8b5cf5
Mark Tran <mark.tran@gmail.com>
2024-11-10 17:22:36 -0800

    add key bindings for dealing with eat scrollback

--- settings/eat.el
+++ settings/eat.el
@@ -5,5 +5,8 @@
 
   :general
   (:keymaps 'eat-semi-char-mode-map
-   "C-d" 'popper-toggle
+   "<home>" 'beginning-of-buffer
+   "<end>" 'end-of-buffer
+   "<prior>" 'evil-scroll-up
+   "<next>" 'evil-scroll-down
    "M-`" 'popper-cycle))
