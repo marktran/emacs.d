85cbad38d3a8b043b13100e958e367b422add544
Mark Tran <mark.tran@gmail.com>
2024-11-10 17:23:01 -0800

    fix bindings for dealing with popper buffer

--- settings/popper.el
+++ settings/popper.el
@@ -9,11 +9,23 @@
   (popper-window-height 0.40)
 
   :general
-  ("M-`" 'popper-cycle)
+  (:keymaps 'popper-mode-map
+   "M-`" 'popper-cycle)
 
   (:prefix "SPC t"
-   "p c" 'popper-cycle
    "p t" 'popper-toggle)
 
   :init
-  (popper-mode 1))
+  (popper-mode 1)
+
+  (defun popper-close-window-hack (&rest _)
+    "Close popper window via `C-g'."
+    ;; `C-g' can deactivate region
+    (when (and (called-interactively-p 'interactive)
+               (not (region-active-p))
+               popper-open-popup-alist)
+      (let ((window (caar popper-open-popup-alist)))
+        (when (window-live-p window)
+          (delete-window window)))))
+  (advice-add #'keyboard-quit :before #'popper-close-window-hack))
+
