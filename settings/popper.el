1774359d33d54eb1edd0f101ba1a8373fd90f915
Mark Tran <mark.tran@gmail.com>
2024-11-10 23:59:39 -0800

    refactor file for clarity

--- settings/popper.el
+++ settings/popper.el
@@ -1,3 +1,12 @@
+(defun popper-close-window-hack (&rest _)
+    "Close popper window via `C-g'."
+    (when (and (called-interactively-p 'interactive)
+               (not (region-active-p))
+               popper-open-popup-alist)
+      (let ((window (caar popper-open-popup-alist)))
+        (when (window-live-p window)
+          (delete-window window)))))
+
 (use-package popper
   :custom
   (popper-reference-buffers
@@ -19,14 +28,6 @@
   :init
   (popper-mode 1)
 
-  (defun popper-close-window-hack (&rest _)
-    "Close popper window via `C-g'."
-    ;; `C-g' can deactivate region
-    (when (and (called-interactively-p 'interactive)
-               (not (region-active-p))
-               popper-open-popup-alist)
-      (let ((window (caar popper-open-popup-alist)))
-        (when (window-live-p window)
-          (delete-window window)))))
+  ;; `C-g' can deactivate region
   (advice-add #'keyboard-quit :before #'popper-close-window-hack))
 
