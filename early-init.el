f4e7e540a93dcdc0ad8ee2953a0fe3799db0b546
Mark Tran <mark.tran@gmail.com>
2024-11-16 05:11:54 -0800

    add early-init.el

---
+++ early-init.el
@@ -0,0 +1,9 @@
+;; Emacs 27 introduces early-init.el, which is run before init.el,
+;; before package and UI initialization happens.
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(when (featurep 'ns)
+  (push '(ns-transparent-titlebar . t) default-frame-alist))
+(setq-default mode-line-format nil)
