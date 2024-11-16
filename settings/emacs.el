4e2043ff3269f5358217312615d292b0aa875b3d
Mark Tran <mark.tran@gmail.com>
2024-11-16 03:24:16 -0800

    update Vertico configuration

---
+++ settings/emacs.el
@@ -0,0 +1,31 @@
+(use-package emacs
+  :after vertico
+  :custom
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (enable-recursive-minibuffers t)
+
+  ;; Hide commands in M-x which do not work in the current mode.  Vertico
+  ;; commands are hidden in normal buffers. This setting is useful beyond
+  ;; Vertico.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (tab-always-indent 'complete)
+
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode))
--- settings/vertico.el
+++ settings/vertico.el
@@ -2,6 +2,9 @@
       (nconc completion-ignored-extensions '(".DS_Store")))
 
 (use-package vertico
+  :custom
+  (vertico-count 15)
+
   :init
   (vertico-mode))
 
@@ -18,6 +21,11 @@
   ;; Tidy shadowed file names
   :hook
   (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
+(use-package vertico-posframe
+  :after vertico
+  :init
+  (vertico-posframe-mode 1))
 
 ;; Persist history over Emacs restarts. Vertico sorts by history position.
 (use-package savehist
@@ -42,32 +50,6 @@
   :init
   (marginalia-mode))
 
-(use-package emacs
-  :after vertico
-  :custom
-  ;; Support opening new minibuffers from inside existing minibuffers.
-  (enable-recursive-minibuffers t)
-  ;; Hide commands in M-x which do not work in the current mode.  Vertico
-  ;; commands are hidden in normal buffers. This setting is useful beyond
-  ;; Vertico.
-  (read-extended-command-predicate #'command-completion-default-include-p)
-  :init
-  ;; Add prompt indicator to `completing-read-multiple'.
-  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-  (defun crm-indicator (args)
-    (cons (format "[CRM%s] %s"
-                  (replace-regexp-in-string
-                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-                   crm-separator)
-                  (car args))
-          (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode))
-
 (use-package orderless
   :after vertico
   :custom
@@ -75,6 +57,6 @@
   ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
   ;; (orderless-component-separator #'orderless-escapable-split-on-space)
   (completion-styles '(orderless basic))
-  (orderless-matching-styles '(orderless-flex orderless-initialism orderless-literal orderless-regexp))
+  (orderless-matching-styles '(orderless-initialism orderless-literal orderless-regexp))
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion)))))
