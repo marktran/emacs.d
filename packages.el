1e54bb743df73db3eaa55843a57abb1cd84f4dc7
Mark Tran <mark.tran@gmail.com>
2024-11-16 22:02:43 -0800

    refactor: Replace package.el with straight.el for package management

--- packages.el
+++ packages.el
@@ -1,12 +1,21 @@
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+; setup straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(unless (file-exists-p "~/.emacs.d/elpa/archives/melpa")
-  (package-refresh-contents))
+;; Install and configure use-package
+(straight-use-package 'use-package)
 
-(setq use-package-always-ensure t
+;; Use straight.el by default with use-package
+(setq straight-use-package-by-default t
       use-package-compute-statistics t
       use-package-verbose t)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-    (package-install 'use-package))
