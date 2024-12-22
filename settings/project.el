7f3bfe1cbb70170c379ac29715ec3e6785e0f577
Mark Tran <mark.tran@gmail.com>
2024-12-22 02:44:52 -0800

    use disproject

--- settings/project.el
+++ settings/project.el
@@ -5,14 +5,15 @@
     (eat)))
 
 (use-package project
-  :ensure nil
+  :ensure nil)
 
-  :general
-  (:prefix "SPC p"
-   "" '(:ignore t :which-key "Project")
-   "." '(project-dired :which-key "Dired")
-   "b" '(consult-project-buffer :which-key "Switch Buffer")
-   "d" '(project-find-dir :which-key "Find Directory")
-   "f" '(project-find-file :which-key "Find File")
-   "p" '(project-switch-project :which-key "Switch Project")
-   "s" '(project-eat-shell :which-key "Shell")))
+(use-package disproject
+  :ensure t
+
+  :custom
+  (disproject-shell-command 'project-eat-shell)
+
+  :config
+  (general-define-key
+   :prefix "SPC"
+   "p" 'disproject-dispatch))
