f5843ffadded8f27accd3853133bf98cbe18de38
Mark Tran <mark.tran@gmail.com>
2024-12-22 18:56:34 -0800

    update notetaking configuration

--- settings/notetaking.el
+++ settings/notetaking.el
@@ -5,6 +5,9 @@
   (org-log-done 'time)
   (org-src-fontify-natively t)
 
+  :hook
+  (org-mode . visual-line-mode)
+
   :general
   (:keymaps 'org-mode-map
    :states 'normal
@@ -40,7 +43,9 @@
    "M-K" 'org-metaup))
 
 (use-package evil-org
+  :ensure t
   :after org
+  :diminish evil-org-mode
 
   :hook
   (org-mode . evil-org-mode)
@@ -48,5 +53,44 @@
   :config
   (evil-org-set-key-theme))
 
+(use-package consult-denote
+  :ensure t
+
+  :config
+  (consult-denote-mode))
+
+(use-package consult-notes
+  :ensure t
+
+  :config
+  (consult-notes-denote-mode))
+
 (use-package denote
-  :ensure t)
+  :ensure t
+
+  :custom
+  (denote-directory (expand-file-name "~/Documents/denote"))
+  (denote-dired-directories-include-subdirectories t)
+  (denote-known-keywords '("interview" "meeting"))
+  (denote-rename-buffer-mode t)
+
+  :hook
+  (dired-mode . denote-dired-mode-in-directories)
+
+  :general
+  (:prefix "SPC n"
+   "" '(:ignore t :which-key "Notes")
+   "c" '(denote-create-note :which-key "Create note")
+   "d" '(denote-journal-extras-new-or-existing-entry :which-key "Jump to daily entry")
+   "f" '(consult-notes :which-key "Find notes")
+   "s" '(consult-denote-grep :which-key "Search notes"))
+
+  :config
+  (setq denote-dired-directories (list denote-directory)))
+
+(use-package denote-journal-extras
+  :ensure nil
+
+  :custom
+  (denote-journal-extras-directory (expand-file-name "journal" denote-directory))
+  (denote-journal-extras-title-format 'day-date-month-year))
