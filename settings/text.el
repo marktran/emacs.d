7138dc14470ab5d4b85fc5aeb75c93dd52b21c60
Mark Tran <mark.tran@gmail.com>
2024-12-22 19:39:40 -0800

    refactor settings for text

--- settings/text.el
+++ settings/text.el
@@ -2,6 +2,23 @@
   :ensure nil
   :diminish flyspell-mode)
 
+(use-package ispell
+  :ensure nil
+
+  :custom
+  (ispell-personal-dictionary "~/.emacs.d/.aspell.en.pws"))
+
+(use-package jinx
+  :ensure t
+  :diminish jinx-mode
+
+  :hook
+  (text-mode . jinx-mode)
+
+  :custom
+  (jinx-ispell-program "aspell")
+  (jinx-ispell-dictionary "en_US"))
+
 (use-package text-mode
   :ensure nil
 
