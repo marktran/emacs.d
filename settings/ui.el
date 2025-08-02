67bbc621a73cec9498e26ca610ca2a532e756f07
Mark Tran <mark.tran@gmail.com>
2025-08-02 03:05:55 -0700

    change ui

--- settings/ui.el
+++ settings/ui.el
@@ -40,7 +40,7 @@
                             :header-line-width 4
                             :mode-line-width 2
                             :tab-width 4
-                            :right-divider-width 30
+                            :right-divider-width 20
                             :scroll-bar-width 8
                             :fringe-width 8)
 
