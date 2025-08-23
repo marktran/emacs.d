dea43974c38f39bd977a8fa61aab3b1bdcf286ff
Mark Tran <mark.tran@gmail.com>
2025-08-23 00:22:59 -0700

    add check for linux

--- platform.el
+++ platform.el
@@ -1,3 +1,5 @@
 (cond
  ((eq system-type 'darwin)
-  (load-file "~/.emacs.d/platform/darwin.el")))
+  (load-file "~/.emacs.d/platform/darwin.el"))
+ ((eq system-type 'gnu/linux)
+  (load-file "~/.emacs.d/platform/linux.el")))
