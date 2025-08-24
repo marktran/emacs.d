a23370259ad01b397116597b8d7eeb058279d4a2
Mark Tran <mark.tran@gmail.com>
2025-08-23 19:01:04 -0700

    add Linux-specific config to initialize default shell and $PATH

--- platform/linux.el
+++ platform/linux.el
@@ -1,4 +1,11 @@
 (add-to-list 'default-frame-alist '(font . "Berkeley Mono-20"))
 
+(let ((user-bin (expand-file-name "~/bin")))
+  (setenv "PATH" (concat user-bin ":" (getenv "PATH")))
+  (add-to-list 'exec-path user-bin))
+
+(setq shell-file-name (executable-find "fish"))
+(setq explicit-shell-file-name shell-file-name)
+
 (setq select-enable-clipboard t)
 (setq select-enable-primary t)
