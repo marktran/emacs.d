a4cc68e14a36cd38079fae17c16286101ab98c6a
Mark Tran <mark.tran@gmail.com>
2025-08-24 23:10:55 -0700

    split out treesitter config

--- settings/javascript.el
+++ settings/javascript.el
@@ -29,16 +29,6 @@
   ((javascript-ts-mode . prettier-mode)
    (typescript-ts-mode . prettier-mode)))
 
-(use-package treesit-auto
-  :ensure t
-
-  :custom
-  (treesit-auto-install t)
-  (treesit-auto-langs '(html javascript json tsx typescript yaml))
-
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode))
 
 (use-package add-node-modules-path
   :ensure t)
---
+++ settings/treesitter.el
@@ -0,0 +1,10 @@
+(use-package treesit-auto
+  :ensure t
+
+  :custom
+  (treesit-auto-install t)
+  (treesit-auto-langs '(html javascript json tsx typescript yaml))
+
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
