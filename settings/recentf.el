b772b1472f689e6a386aad7373d7a67f2216da16
Mark Tran <mark.tran@gmail.com>
2025-12-13 15:53:21 -0800

    adjust recentf settings

--- settings/recentf.el
+++ settings/recentf.el
@@ -3,13 +3,14 @@
 
   :custom
   (recentf-auto-cleanup 'never)
+  (recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list))
   (recentf-exclude (list (expand-file-name package-user-dir)
                          #'ignoramus-boring-p
                          ".bookmarks"
                          "recentf"
                          "TAGS"))
-  (recentf-keep '(file-remote-p file-readable-p))
-  (recentf-max-saved-items 15)
+  (recentf-keep '(file-remote-p))
+  (recentf-max-saved-items 150)
   (recentf-filename-handlers '(file-truename))  ;; Resolve symlinks
 
   :config
