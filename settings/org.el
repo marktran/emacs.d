db0ae3f1ba556dca0217bb22ec2f94b43be8caf5
Mark Tran <mark.tran@gmail.com>
2026-02-16 13:21:50 -0800

    clean up modeline for org-agenda

--- settings/org.el
+++ settings/org.el
@@ -8,4 +8,19 @@
    '(("t" "Task" entry (file org-default-notes-file)
       "* TODO %?\n  %U\n  %a\n  %i")))
 
-  (org-agenda-files '("~/Dropbox/org/now/inbox.org")))
+  (org-agenda-files '("~/Dropbox/org/now/inbox.org"))
+
+  :config
+  (defun m/org-agenda-hide-ddl-and-grid (&rest _)
+    "Hide Ddl and Grid status markers from the org-agenda mode line."
+    (when (and (eq major-mode 'org-agenda-mode)
+               (listp mode-name))
+      (let (cleaned)
+        (dolist (part mode-name)
+          (unless (member part '(" Ddl" " Grid"))
+            (push part cleaned)))
+        (setq mode-name (nreverse cleaned))
+        (force-mode-line-update))))
+
+  (with-eval-after-load 'org-agenda
+    (advice-add 'org-agenda-set-mode-name :after #'m/org-agenda-hide-ddl-and-grid)))
