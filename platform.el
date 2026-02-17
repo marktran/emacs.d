ed0b17676c5faa44ae83ffb2230e13a3a97946b3
Mark Tran <mark.tran@gmail.com>
2026-02-16 20:51:35 -0800

    Rename platform directory to os

--- platform.el
+++ platform.el
@@ -1,5 +1,5 @@
 (cond
  ((eq system-type 'darwin)
-  (load-file "~/.emacs.d/platform/darwin.el"))
+  (load-file "~/.emacs.d/os/darwin.el"))
  ((eq system-type 'gnu/linux)
-  (load-file "~/.emacs.d/platform/linux.el")))
+  (load-file "~/.emacs.d/os/linux.el")))
