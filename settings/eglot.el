f4fa9df9011ef90030842eec89aa9f9fe55c7382
Mark Tran <mark.tran@gmail.com>
2026-03-14 03:25:31 -0700

    fix(eglot): only start TypeScript LSP in JS/TS projects

--- settings/eglot.el
+++ settings/eglot.el
@@ -1,18 +1,28 @@
-(defun mark/project-root (&optional project)
+(defun m/project-root (&optional project)
   "Return root directory for PROJECT or current project."
   (when-let* ((proj (or project (project-current))))
     (project-root proj)))
 
-(defun mark/find-project-bin (name &optional project)
+(defun m/find-project-bin (name &optional project)
   "Find executable NAME in project-local node_modules/.bin, if present."
-  (when-let* ((root (mark/project-root project))
+  (when-let* ((root (m/project-root project))
               (bin (expand-file-name (concat "node_modules/.bin/" name) root))
               ((file-executable-p bin)))
     bin))
 
-(defun mark/find-project-tsserver (&optional project)
+(defun m/typescript-project-p (&optional project)
+  "Return non-nil when PROJECT or current project looks like a TS/JS project."
+  (when-let* ((root (m/project-root project)))
+    (catch 'found
+      (dolist (name '("tsconfig.json" "jsconfig.json" "package.json"
+                      "deno.json" "deno.jsonc"))
+        (when (file-exists-p (expand-file-name name root))
+          (throw 'found t)))
+      nil)))
+
+(defun m/find-project-tsserver (&optional project)
   "Find a project-local tsserver.js path for PROJECT."
-  (when-let* ((root (mark/project-root project)))
+  (when-let* ((root (m/project-root project)))
     (or
      ;; npm/yarn layout
      (let ((npm-tsserver (expand-file-name "node_modules/typescript/lib/tsserver.js" root)))
@@ -26,12 +36,12 @@
                "node_modules/typescript/lib/tsserver\\.js$"
                nil)))))))
 
-(defun mark/typescript-eglot-contact (_interactive project)
+(defun m/typescript-eglot-contact (_interactive project)
   "Build Eglot contact for TypeScript buffers in PROJECT."
   (let* ((typescript-language-server
-          (or (mark/find-project-bin "typescript-language-server" project)
+          (or (m/find-project-bin "typescript-language-server" project)
               (executable-find "typescript-language-server")))
-         (tsserver-path (mark/find-project-tsserver project)))
+         (tsserver-path (m/find-project-tsserver project)))
     (when typescript-language-server
       (if tsserver-path
           (list typescript-language-server
@@ -39,6 +49,11 @@
                 :initializationOptions
                 `(:tsserver (:path ,tsserver-path)))
         (list typescript-language-server "--stdio")))))
+
+(defun m/maybe-typescript-eglot ()
+  "Start Eglot only when the current project looks like a TS/JS project."
+  (when (m/typescript-project-p)
+    (eglot-ensure)))
 
 (use-package eglot
   :ensure nil
@@ -49,7 +64,7 @@
   (eglot-autoshutdown t)
   (eglot-send-changes-idle-time 0.5)
 
-  :hook (typescript-ts-mode . eglot-ensure)
+  :hook (typescript-ts-mode . m/maybe-typescript-eglot)
 
   :config
   (when (executable-find "emacs-lsp-booster")
@@ -63,4 +78,4 @@
                   eglot-server-programs)))
 
   (setf (alist-get 'typescript-ts-mode eglot-server-programs)
-        #'mark/typescript-eglot-contact))
+        #'m/typescript-eglot-contact))
