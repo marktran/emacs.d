1667d89b023a6dae5a804bd17ddf5af2638e98f9
Mark Tran <mark.tran@gmail.com>
2026-07-11 17:55:02 -0700

    fix(recentf): silence periodic save messages

--- settings/recentf.el
+++ settings/recentf.el
@@ -1,5 +1,14 @@
+(defvar recentf-auto-save-timer nil
+  "Idle timer used to save the recent file list.")
+
 (use-package recentf
   :ensure nil
+
+  :preface
+  (defun m/recentf-save-list-silently ()
+    "Save the recent file list without displaying minibuffer messages."
+    (let ((inhibit-message t))
+      (recentf-save-list)))
 
   :custom
   (recentf-auto-cleanup 'never)
@@ -13,6 +22,8 @@
   (recentf-filename-handlers '(file-truename))  ;; Resolve symlinks
 
   :config
+  (when (timerp (bound-and-true-p recentf-auto-save-timer))
+    (cancel-timer recentf-auto-save-timer))
   (setq recentf-auto-save-timer
-        (run-with-idle-timer 600 t #'recentf-save-list))
+        (run-with-idle-timer 600 t #'m/recentf-save-list-silently))
   (recentf-mode 1))
