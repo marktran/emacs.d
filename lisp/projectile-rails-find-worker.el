b61be0df213476d5e9e891cfd1e2f9e5638eef29
Mark Tran <mark.tran@gmail.com>
2026-07-19 01:54:02 -0700

    chore(lisp): remove unused projectile-rails worker finder

--- lisp/projectile-rails-find-worker.el
+++
@@ -1,6 +0,0 @@
-(defun projectile-rails-find-worker ()
-  (interactive)
-  (projectile-rails-find-resource
-   "worker: "
-   '(("app/workers/" "/workers/\\(.+?\\)\\(_worker\\)?\\.rb\\'"))
-   "app/workers/${filename}_worker.rb"))
