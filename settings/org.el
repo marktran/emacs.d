e6ee15920e2f70438672910bf672c384dc8cdb11
Mark Tran <mark.tran@gmail.com>
2026-07-19 02:00:20 -0700

    refactor(org): dedupe bindings and simplify agenda mode-line advice
    Derive org-agenda-files from org-default-notes-file, drop the
    org-src-fontify-natively setting (default since Org 9), merge the
    M-* bindings shared by normal and insert states into one block, and
    simplify the Ddl/Grid mode-line cleanup.

--- settings/org.el
+++ settings/org.el
@@ -3,19 +3,25 @@
 
   :custom
   (org-default-notes-file "~/Dropbox/org/now/inbox.org")
+  (org-agenda-files (list org-default-notes-file))
   (org-log-done 'time)
-  (org-src-fontify-natively t)
 
   (org-capture-templates
    '(("t" "Task" entry (file org-default-notes-file)
       "* TODO %?\n  %U\n  %a\n  %i")))
 
-  (org-agenda-files '("~/Dropbox/org/now/inbox.org"))
-
   :hook
   (org-mode . visual-line-mode)
 
   :general
+  (:keymaps 'org-mode-map
+   :states '(normal insert)
+   "M-j" 'org-shiftleft
+   "M-k" 'org-shiftright
+   "M-H" 'org-metaleft
+   "M-J" 'org-metadown
+   "M-L" 'org-metaright
+   "M-K" 'org-metaup)
   (:keymaps 'org-mode-map
    :states 'normal
    "za" 'org-cycle
@@ -24,37 +30,20 @@
    "zm" 'outline-hide-body
    "zo" 'outline-show-subtree
    "zr" 'outline-show-all
-   "RET" 'org-open-at-point
-   "M-j" 'org-shiftleft
-   "M-k" 'org-shiftright
-   "M-H" 'org-metaleft
-   "M-J" 'org-metadown
-   "M-L" 'org-metaright
-   "M-K" 'org-metaup)
+   "RET" 'org-open-at-point)
   (:keymaps 'org-mode-map
    :states 'visual
    "s-v" 'org-insert-link-dwim)
   (:keymaps 'org-mode-map
    :states 'insert
-   "s-v" 'org-yank-dwim
-   "M-j" 'org-shiftleft
-   "M-k" 'org-shiftright
-   "M-H" 'org-metaleft
-   "M-J" 'org-metadown
-   "M-L" 'org-metaright
-   "M-K" 'org-metaup)
+   "s-v" 'org-yank-dwim)
 
   :config
   (defun m/org-agenda-hide-ddl-and-grid (&rest _)
     "Hide Ddl and Grid status markers from the org-agenda mode line."
     (when (and (eq major-mode 'org-agenda-mode)
                (listp mode-name))
-      (let (cleaned)
-        (dolist (part mode-name)
-          (unless (member part '(" Ddl" " Grid"))
-            (push part cleaned)))
-        (setq mode-name (nreverse cleaned))
-        (force-mode-line-update))))
+      (setq mode-name (remove " Ddl" (remove " Grid" mode-name)))
+      (force-mode-line-update)))
 
-  (with-eval-after-load 'org-agenda
-    (advice-add 'org-agenda-set-mode-name :after #'m/org-agenda-hide-ddl-and-grid)))
+  (advice-add 'org-agenda-set-mode-name :after #'m/org-agenda-hide-ddl-and-grid))
