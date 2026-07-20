f78afd3399c635b7fcf575968aa10632b19ec203
Mark Tran <mark.tran@gmail.com>
2026-07-19 20:13:43 -0700

    refactor(lisp): set lisp-indent-function instead of redefining it
    Define m/lisp-indent-function and point the lisp-indent-function
    variable at it rather than clobbering the built-in inside
    with-eval-after-load. Extract the emacs-lisp-mode hook lambda into a
    named m/emacs-lisp-setup, declare calculate-lisp-indent-last-sexp, and
    condense the copied docstring down to the keyword-list behavior it
    changes.

--- settings/lisp.el
+++ settings/lisp.el
@@ -1,11 +1,14 @@
 (use-package elisp-mode
   :ensure nil
 
+  :preface
+  (defun m/emacs-lisp-setup ()
+    (setq-local mode-name "ELisp")
+    (setq-local sentence-end-double-space nil))
+
   :hook
   ((emacs-lisp-mode . show-paren-mode)
-   (emacs-lisp-mode . (lambda ()
-                        (setq mode-name "ELisp")
-                        (setq-local sentence-end-double-space nil))))
+   (emacs-lisp-mode . m/emacs-lisp-setup))
 
   :general
   (:keymaps 'emacs-lisp-mode-map
@@ -13,7 +16,7 @@
    :prefix "SPC ,"
    "" '(:ignore t :which-key "ELisp")
    "b" '(eval-buffer :which-key "Eval buffer")
-   "d" '(eval-defun :which-key "Eval Defun")
+   "d" '(eval-defun :which-key "Eval defun")
    "e" '(eval-last-sexp :which-key "Eval sexp before point")
    "r" '(eval-region :which-key "Eval region")))
 
@@ -28,87 +31,71 @@
    (scheme-mode . paredit-mode)
    (slime-repl-mode . paredit-mode)))
 
-;; https://github.com/Fuco1/.emacs.d/blob/master/site-lisp/my-redef.el#L18
-;; redefines the silly indent of keyword lists
-;; before
-;;   (:foo bar
-;;         :baz qux)
-;; after
-;;   (:foo bar
-;;    :baz qux)
-(with-eval-after-load 'lisp-mode
-  (defun lisp-indent-function (indent-point state)
-     "This function is the normal value of the variable `lisp-indent-function'.
-The function `calculate-lisp-indent' calls this to determine
-if the arguments of a Lisp function call should be indented specially.
+(defvar calculate-lisp-indent-last-sexp) ; dynamically bound in `calculate-lisp-indent'
+
+(defun m/lisp-indent-function (indent-point state)
+  "Like `lisp-indent-function', but indent keyword lists sanely.
+
+Instead of the default
+
+  (:foo bar
+        :baz qux)
+
+keyword lists are indented as
+
+  (:foo bar
+   :baz qux)
 
 INDENT-POINT is the position at which the line being indented begins.
-Point is located at the point to indent under (for default indentation);
 STATE is the `parse-partial-sexp' state for that position.
 
-If the current line is in a call to a Lisp function that has a non-nil
-property `lisp-indent-function' (or the deprecated `lisp-indent-hook'),
-it specifies how to indent.  The property value can be:
+Adapted from URL
+`https://github.com/Fuco1/.emacs.d/blob/master/site-lisp/my-redef.el'."
+  (let ((normal-indent (current-column))
+        (orig-point (point)))
+    (goto-char (1+ (elt state 1)))
+    (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
+    (cond
+     ;; car of form doesn't seem to be a symbol, or is a keyword
+     ((and (elt state 2)
+           (or (not (looking-at "\\sw\\|\\s_"))
+               (looking-at ":")))
+      (unless (> (save-excursion (forward-line 1) (point))
+                 calculate-lisp-indent-last-sexp)
+        (goto-char calculate-lisp-indent-last-sexp)
+        (beginning-of-line)
+        (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t))
+      ;; Indent under the list or under the first sexp on the same
+      ;; line as calculate-lisp-indent-last-sexp.  Note that first
+      ;; thing on that line has to be complete sexp since we are
+      ;; inside the innermost containing sexp.
+      (backward-prefix-chars)
+      (current-column))
+     ((and (save-excursion
+             (goto-char indent-point)
+             (skip-syntax-forward " ")
+             (not (looking-at ":")))
+           (save-excursion
+             (goto-char orig-point)
+             (looking-at ":")))
+      (save-excursion
+        (goto-char (+ 2 (elt state 1)))
+        (current-column)))
+     (t
+      (let* ((function (buffer-substring (point)
+                                         (progn (forward-sexp 1) (point))))
+             (method (or (function-get (intern-soft function)
+                                       'lisp-indent-function)
+                         (get (intern-soft function) 'lisp-indent-hook))))
+        (cond ((or (eq method 'defun)
+                   (and (null method)
+                        (> (length function) 3)
+                        (string-match "\\`def" function)))
+               (lisp-indent-defform state indent-point))
+              ((integerp method)
+               (lisp-indent-specform method state
+                                     indent-point normal-indent))
+              (method
+               (funcall method indent-point state))))))))
 
-* `defun', meaning indent `defun'-style
-  \(this is also the case if there is no property and the function
-  has a name that begins with \"def\", and three or more arguments);
-
-* an integer N, meaning indent the first N arguments specially
-  (like ordinary function arguments), and then indent any further
-  arguments like a body;
-
-* a function to call that returns the indentation (or nil).
-  `lisp-indent-function' calls this function with the same two arguments
-  that it itself received.
-
-This function returns either the indentation to use, or nil if the
-Lisp function does not specify a special indentation."
-     (let ((normal-indent (current-column))
-           (orig-point (point)))
-       (goto-char (1+ (elt state 1)))
-       (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
-       (cond
-        ;; car of form doesn't seem to be a symbol, or is a keyword
-        ((and (elt state 2)
-              (or (not (looking-at "\\sw\\|\\s_"))
-                  (looking-at ":")))
-         (if (not (> (save-excursion (forward-line 1) (point))
-                     calculate-lisp-indent-last-sexp))
-             (progn (goto-char calculate-lisp-indent-last-sexp)
-                    (beginning-of-line)
-                    (parse-partial-sexp (point)
-                                        calculate-lisp-indent-last-sexp 0 t)))
-         ;; Indent under the list or under the first sexp on the same
-         ;; line as calculate-lisp-indent-last-sexp.  Note that first
-         ;; thing on that line has to be complete sexp since we are
-         ;; inside the innermost containing sexp.
-         (backward-prefix-chars)
-         (current-column))
-        ((and (save-excursion
-                (goto-char indent-point)
-                (skip-syntax-forward " ")
-                (not (looking-at ":")))
-              (save-excursion
-                (goto-char orig-point)
-                (looking-at ":")))
-         (save-excursion
-           (goto-char (+ 2 (elt state 1)))
-           (current-column)))
-        (t
-         (let ((function (buffer-substring (point)
-                                           (progn (forward-sexp 1) (point))))
-               method)
-           (setq method (or (function-get (intern-soft function)
-                                          'lisp-indent-function)
-                            (get (intern-soft function) 'lisp-indent-hook)))
-           (cond ((or (eq method 'defun)
-                      (and (null method)
-                           (> (length function) 3)
-                           (string-match "\\`def" function)))
-                 (lisp-indent-defform state indent-point))
-                 ((integerp method)
-                  (lisp-indent-specform method state
-                                        indent-point normal-indent))
-                 (method
-                  (funcall method indent-point state)))))))))
+(setq-default lisp-indent-function #'m/lisp-indent-function)
